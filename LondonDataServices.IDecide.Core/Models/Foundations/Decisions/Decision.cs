024685070aefdd12d68235f3a5a349a6c248ed6b
Joe Owens <joe.owens@answerdigital.com>
2025-07-24 17:45:00 +0100

    DATA: Decision

--- LondonDataServices.IDecide.Core/Models/Foundations/Decisions/Decision.cs
+++ LondonDataServices.IDecide.Core/Models/Foundations/Decisions/Decision.cs
@@ -3,22 +3,22 @@
 // ---------------------------------------------------------
 
 using System;
+using LondonDataServices.IDecide.Core.Models.Foundations.DecisionTypes;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.Collections.Generic;
-using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
 
-namespace LondonDataServices.IDecide.Core.Models.Foundations.DecisionTypes
+namespace LondonDataServices.IDecide.Core.Models.Foundations.Decisions
 {
-    public class DecisionType
+    public class Decision
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string PatientNhsNumber { get; set; }
+        public Guid DecisionTypeId { get; set; }
         public string CreatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public string UpdatedBy { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
 
         [BindNever]
-        public List<Decision> Decisions { get; set; } = new List<Decision>();
+        public DecisionType DecisionType { get; set; } = null!;
     }
 }
