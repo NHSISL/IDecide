850b925bb00fda94a12a380f3d535c7f54784f44
Joe Owens <joe.owens@answerdigital.com>
2025-07-28 12:37:39 +0100

    CODE-RUB: Readd commented out line

--- LondonDataServices.IDecide.Core/Brokers/Storages/Sql/StorageBroker.cs
+++ LondonDataServices.IDecide.Core/Brokers/Storages/Sql/StorageBroker.cs
@@ -23,7 +23,7 @@ namespace LondonDataServices.IDecide.Core.Brokers.Storages.Sql
         public StorageBroker(IConfiguration configuration)
         {
             this.configuration = configuration;
-            //Database.Migrate();
+            Database.Migrate();
             efCoreClient = new EFCoreClient(this);
         }
 
