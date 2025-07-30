73c80b8f33ec74416c361dddaf6901365f8d9f7b
Joe Owens <joe.owens@answerdigital.com>
2025-07-30 13:35:07 +0100

    Removing Audit references from Decision migration

--- LondonDataServices.IDecide.Core/Migrations/20250730121924_AddDecisionTypeAndDecision.cs
+++ LondonDataServices.IDecide.Core/Migrations/20250730121924_AddDecisionTypeAndDecision.cs
@@ -12,32 +12,7 @@ namespace LondonDataServices.IDecide.Core.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.EnsureSchema(
-                name: "Audit");
-
-            migrationBuilder.EnsureSchema(
                 name: "Decision");
-
-            migrationBuilder.CreateTable(
-                name: "Audits",
-                schema: "Audit",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    CorrelationId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    AuditType = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    FileName = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
-                    LogLevel = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
-                    CreatedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    UpdatedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
-                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Audits", x => x.Id);
-                });
 
             migrationBuilder.CreateTable(
                 name: "DecisionTypes",
@@ -82,24 +57,6 @@ namespace LondonDataServices.IDecide.Core.Migrations
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Audits_AuditType",
-                schema: "Audit",
-                table: "Audits",
-                column: "AuditType");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Audits_CorrelationId",
-                schema: "Audit",
-                table: "Audits",
-                column: "CorrelationId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Audits_LogLevel",
-                schema: "Audit",
-                table: "Audits",
-                column: "LogLevel");
-
-            migrationBuilder.CreateIndex(
                 name: "IX_Decisions_DecisionTypeId",
                 schema: "Decision",
                 table: "Decisions",
@@ -109,10 +66,6 @@ namespace LondonDataServices.IDecide.Core.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Audits",
-                schema: "Audit");
-
             migrationBuilder.DropTable(
                 name: "Decisions",
                 schema: "Decision");
