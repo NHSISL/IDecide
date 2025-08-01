fdae5c3392d99bdf28d514614364e5e633b3d633
David Cunliffe <david.cunliffe2@nhs.net>
2025-08-01 14:46:38 +0100

    infra: adding debug info for webserver

--- LondonDataServices.IDecide.Portal.Client/playwright.config.ts
+++ LondonDataServices.IDecide.Portal.Client/playwright.config.ts
@@ -6,15 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const authFile = path.join(__dirname, './playwright/.auth/user.json');
+const webServerCommand = 'dotnet run --project ..\\LondonDataServices.IDecide.Portal.Server\\LondonDataServices.IDecide.Portal.Server.csproj' + (process.env.CI ? ' --environment ASPNETCORE_ENVIRONMENT=ContinuousIntegration' : '');
+console.log(webServerCommand);
 
 export default defineConfig({
     testDir: './tests',
     reporter: 'html',
     webServer: {
-        command: 'dotnet run -p ..\\LondonDataServices.IDecide.Portal.Server\\LondonDataServices.IDecide.Portal.Server.csproj' + (process.env.CI ? ' --environment ASPNETCORE_ENVIRONMENT=ContinuousIntegration' : ''),
+        command: webServerCommand,
         url: 'https://localhost:5173',
-        reuseExistingServer: true,
+        reuseExistingServer: !process.env.CI,
         ignoreHTTPSErrors: true,
+        stdout: 'pipe'
     },
     use: {
         baseURL: 'http://localhost:5173',
