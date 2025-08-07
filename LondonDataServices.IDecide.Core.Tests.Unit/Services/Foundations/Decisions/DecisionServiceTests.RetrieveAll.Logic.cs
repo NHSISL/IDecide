65efadd2cb6cca63532be6a496fe11c9ce3766c8
Christo du Toit <christo@dutoit.co.uk>
2025-08-07 23:30:09 +0100

    FOUNDATIONS: Decisions

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Decisions/DecisionServiceTests.RetrieveAll.Logic.cs
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Decisions
+{
+    public partial class DecisionServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnDecisions()
+        {
+            // given
+            IQueryable<Decision> randomDecisions = CreateRandomDecisions();
+            IQueryable<Decision> storageDecisions = randomDecisions;
+            IQueryable<Decision> expectedDecisions = storageDecisions;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllDecisionsAsync())
+                    .ReturnsAsync(storageDecisions);
+
+            // when
+            IQueryable<Decision> actualDecisions =
+                await this.decisionService.RetrieveAllDecisionsAsync();
+
+            // then
+            actualDecisions.Should().BeEquivalentTo(expectedDecisions);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllDecisionsAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
