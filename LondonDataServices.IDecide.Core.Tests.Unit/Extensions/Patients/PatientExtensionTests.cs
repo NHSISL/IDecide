e5e46345b1f05ac66bc44981db6ad5a7374ad181
Joe Owens <joe.owens@answerdigital.com>
2025-08-12 15:07:31 +0100

    Adding extra logic test for PatientExtension Redact method

--- LondonDataServices.IDecide.Core.Tests.Unit/Extensions/Patients/PatientExtensionTests.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Extensions/Patients/PatientExtensionTests.cs
@@ -129,5 +129,57 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Extensions.Patients
                 ValidationCodeExpiresOn = patient.ValidationCodeExpiresOn
             };
         }
+
+        public Patient GetPatientToRedactWithPostcodeInAddress()
+        {
+            return new Patient
+            {
+                Address = "16 Red Grove, Testville, AB2 1ZY",
+                DateOfBirth = GetRandomDateTimeOffset(),
+                Email = "test.family@gmail.com",
+                GivenName = "Test Person",
+                NhsNumber = GenerateRandom10DigitNumber(),
+                Phone = "07123456712",
+                PostCode = "AB2 1ZY",
+                Surname = "Family",
+                CreatedBy = GetRandomString(),
+                CreatedDate = GetRandomDateTimeOffset(),
+                Decisions = new List<Decision>(),
+                Gender = GetRandomString(),
+                Id = Guid.NewGuid(),
+                RetryCount = 0,
+                Title = GetRandomString(),
+                UpdatedBy = GetRandomString(),
+                UpdatedDate = GetRandomDateTimeOffset(),
+                ValidationCode = GetRandomString(),
+                ValidationCodeExpiresOn = GetRandomDateTimeOffset()
+            };
+        }
+
+        public Patient GetRedactedPatientWithPostcodeInAddress(Patient patient)
+        {
+            return new Patient
+            {
+                Address = "16 R** G****, T********, A** 1**",
+                DateOfBirth = patient.DateOfBirth,
+                Email = "t***.f*****@gmail.com",
+                GivenName = "T*** P*****",
+                NhsNumber = patient.NhsNumber,
+                Phone = "07*******12",
+                PostCode = "AB2 1**",
+                Surname = "F*****",
+                CreatedBy = patient.CreatedBy,
+                CreatedDate = patient.CreatedDate,
+                Decisions = patient.Decisions,
+                Gender = patient.Gender,
+                Id = patient.Id,
+                RetryCount = patient.RetryCount,
+                Title = patient.Title,
+                UpdatedBy = patient.UpdatedBy,
+                UpdatedDate = patient.UpdatedDate,
+                ValidationCode = patient.ValidationCode,
+                ValidationCodeExpiresOn = patient.ValidationCodeExpiresOn
+            };
+        }
     }
 }
