fe665c2df4f24c9f40d640a2618bb4bd81b935b9
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-14 12:45:43 +0100

    ShouldSendCodeNotificationAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core/Models/Foundations/Notifications/NotificationInfo.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Patients;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LondonDataServices.IDecide.Core.Models.Foundations.Notifications
+{
+    public class NotificationInfo
+    {
+        [BindNever]
+        public Decision Decision { get; set; } = null!;
+
+        [BindNever]
+        public Patient Patient { get; set; } = null!;
+    }
+}
