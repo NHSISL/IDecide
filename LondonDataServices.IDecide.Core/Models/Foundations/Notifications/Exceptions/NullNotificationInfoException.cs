29338d11224e625c3f9f17647ef383686dc46289
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-14 15:55:47 +0100

    ShouldThrowValidationExceptionOnSendCodeNotificationWhenNotificationInfoIsNullAndLogItAsync-> FAIL

---
+++ LondonDataServices.IDecide.Core/Models/Foundations/Notifications/Exceptions/NullNotificationInfoException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Foundations.Notifications.Exceptions
+{
+    public class NullNotificationInfoException : Xeption
+    {
+        public NullNotificationInfoException(string message)
+            : base(message: message)
+        {
+        }
+    }
+}
