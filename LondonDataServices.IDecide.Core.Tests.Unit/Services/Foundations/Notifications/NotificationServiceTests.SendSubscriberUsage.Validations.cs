c5475e16819ab8d0b163ca8ab0668875c16de3ca
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-19 10:41:49 +0100

    ShouldThrowValidationExceptionOnSendSubscriberUsageNotificationWhenNotificationInfoIsNullAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendSubscriberUsage.Validations.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications.Exceptions;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Notifications
+{
+    public partial class NotificationServiceTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnSendSubscriberUsageNotificationWhenNotificationInfoIsNullAndLogItAsync()
+        {
+            // given
+            NotificationInfo nullNotificationInfo = null;
+
+            var nullNotificationInfoException =
+                new NullNotificationInfoException(message: "Notification info is null.");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation errors occurred, please try again.",
+                    innerException: nullNotificationInfoException);
+
+            // when
+            ValueTask sendSubscriberUsageNotificationTask =
+                this.notificationService.SendSubscriberUsageNotificationAsync(nullNotificationInfo);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(
+                    () => sendSubscriberUsageNotificationTask.AsTask());
+
+            // then
+            actualNotificationValidationException.Should().BeEquivalentTo(expectedNotificationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                    broker.LogErrorAsync(It.Is(SameExceptionAs(
+                        expectedNotificationValidationException))),
+                Times.Once);
+
+            this.notificationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
