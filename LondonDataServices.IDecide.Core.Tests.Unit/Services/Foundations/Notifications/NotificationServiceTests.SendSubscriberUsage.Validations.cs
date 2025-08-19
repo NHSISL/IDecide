6be9d5209e69aa6016d4d46e2a791f4113961369
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-19 10:44:42 +0100

    ShouldThrowValidationExceptionOnSendSubscriberUsageNotificationIfNotificationInfoIsInvalidAndLogItAsync -> PASS

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendSubscriberUsage.Validations.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendSubscriberUsage.Validations.cs
@@ -4,8 +4,11 @@
 
 using System.Threading.Tasks;
 using FluentAssertions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Foundations.DecisionTypes;
 using LondonDataServices.IDecide.Core.Models.Foundations.Notifications;
 using LondonDataServices.IDecide.Core.Models.Foundations.Notifications.Exceptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Patients;
 using Moq;
 
 namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Notifications
@@ -42,6 +45,135 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Notifi
                     broker.LogErrorAsync(It.Is(SameExceptionAs(
                         expectedNotificationValidationException))),
                 Times.Once);
+
+            this.notificationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task
+            ShouldThrowValidationExceptionOnSendSubscriberUsageNotificationIfNotificationInfoIsInvalidAndLogItAsync(
+                string invalidText)
+        {
+            // given
+            NotificationPreference invalidNotificationPreference = (NotificationPreference)999;
+
+            var invalidNotificationInfo = new NotificationInfo
+            {
+                Patient = new Patient
+                {
+                    NhsNumber = invalidText,
+                    Title = invalidText,
+                    GivenName = invalidText,
+                    Surname = invalidText,
+                    Gender = invalidText,
+                    Email = invalidText,
+                    Phone = invalidText,
+                    Address = invalidText,
+                    PostCode = invalidText,
+                    ValidationCode = invalidText,
+                    NotificationPreference = invalidNotificationPreference,
+                },
+                Decision = new Decision
+                {
+                    DecisionChoice = invalidText,
+                    ResponsiblePersonGivenName = invalidText,
+                    ResponsiblePersonSurname = invalidText,
+                    ResponsiblePersonRelationship = invalidText,
+                    DecisionType = new DecisionType
+                    {
+                        Name = invalidText
+                    }
+                }
+            };
+
+            var invalidArgumentsNotificationException =
+                new InvalidArgumentsNotificationException(
+                    message: "Invalid notification arguments. Please correct the errors and try again.");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.NhsNumber),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.Title),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.GivenName),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.Surname),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.DateOfBirth),
+                values: "Date is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.Gender),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.Email),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.Phone),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.Address),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.PostCode),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.ValidationCode),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.ValidationCodeExpiresOn),
+                values: "Date is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Patient.NotificationPreference),
+                values: "Value is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Decision.DecisionChoice),
+                values: "Text is required");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationInfo.Decision.DecisionType.Name),
+                values: "Text is required");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation errors occurred, please try again.",
+                    innerException: invalidArgumentsNotificationException);
+
+            // when
+            ValueTask sendSubscriberUsageNotificationTask =
+                this.notificationService.SendSubscriberUsageNotificationAsync(invalidNotificationInfo);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(
+                    () => sendSubscriberUsageNotificationTask.AsTask());
+
+            // then
+            actualNotificationValidationException.Should().BeEquivalentTo(expectedNotificationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNotificationValidationException))),
+                        Times.Once);
 
             this.notificationBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
