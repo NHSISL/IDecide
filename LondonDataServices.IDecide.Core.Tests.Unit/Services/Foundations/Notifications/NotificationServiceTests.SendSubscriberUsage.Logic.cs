3eb2d36f0da5946216b8943d0a1856829efa224b
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-19 10:30:59 +0100

    ShouldSendSubscriberUsageNotificationAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendSubscriberUsage.Logic.cs
@@ -0,0 +1,103 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Notifications
+{
+    public partial class NotificationServiceTests
+    {
+        [Theory]
+        [InlineData(NotificationPreference.Email)]
+        [InlineData(NotificationPreference.Letter)]
+        [InlineData(NotificationPreference.Sms)]
+        public async Task ShouldSendSubscriberUsageNotificationAsync(NotificationPreference notificationPreference)
+        {
+            // given
+            NotificationInfo randomNotificationInfo = CreateRandomNotificationInfo();
+            randomNotificationInfo.Patient.NotificationPreference = notificationPreference;
+            NotificationInfo inputNotificationInfo = randomNotificationInfo;
+            Dictionary<string, dynamic> personalisation = GetPersonalisation(inputNotificationInfo);
+
+            string result = GetRandomString();
+
+            switch (notificationPreference)
+            {
+                case NotificationPreference.Email:
+                    this.notificationConfig.EmailSubscriberUsageTemplateId = GetRandomString();
+
+                    this.notificationBrokerMock.Setup(broker =>
+                        broker.SendEmailAsync(
+                            inputNotificationInfo.Patient.Email,
+                            this.notificationConfig.EmailSubscriberUsageTemplateId,
+                            personalisation))
+                        .ReturnsAsync(result);
+
+                    break;
+
+                case NotificationPreference.Sms:
+                    this.notificationConfig.SmsSubscriberUsageTemplateId = GetRandomString();
+
+                    this.notificationBrokerMock.Setup(broker =>
+                        broker.SendSmsAsync(
+                            this.notificationConfig.SmsSubscriberUsageTemplateId, personalisation))
+                        .ReturnsAsync(result);
+
+                    break;
+
+                case NotificationPreference.Letter:
+                    this.notificationConfig.LetterSubscriberUsageTemplateId = GetRandomString();
+
+                    this.notificationBrokerMock.Setup(broker =>
+                        broker.SendLetterAsync(
+                            this.notificationConfig.LetterSubscriberUsageTemplateId,
+                            personalisation,
+                            string.Empty))
+                        .ReturnsAsync(result);
+
+                    break;
+            }
+
+            // when
+            await this.notificationService.SendCodeNotificationAsync(notificationInfo: inputNotificationInfo);
+
+            // then
+            switch (notificationPreference)
+            {
+                case NotificationPreference.Email:
+                    this.notificationBrokerMock.Verify(broker =>
+                        broker.SendEmailAsync(
+                            inputNotificationInfo.Patient.Email,
+                            this.notificationConfig.EmailSubscriberUsageTemplateId,
+                            personalisation),
+                        Times.Once);
+
+                    break;
+
+                case NotificationPreference.Sms:
+                    this.notificationBrokerMock.Verify(broker =>
+                        broker.SendSmsAsync(
+                            this.notificationConfig.SmsSubscriberUsageTemplateId, personalisation),
+                        Times.Once);
+
+                    break;
+
+                case NotificationPreference.Letter:
+                    this.notificationBrokerMock.Verify(broker =>
+                        broker.SendLetterAsync(
+                            notificationConfig.LetterSubscriberUsageTemplateId,
+                            personalisation,
+                            string.Empty),
+                        Times.Once);
+
+                    break;
+            }
+
+            this.notificationBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
