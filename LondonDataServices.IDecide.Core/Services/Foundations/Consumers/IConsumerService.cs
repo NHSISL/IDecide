da54f4c77786a91361080a81923fe0ea808d6bef
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-21 13:19:33 +0100

    ShouldReturnConsumers -> FAIL

--- LondonDataServices.IDecide.Core/Services/Foundations/Consumers/IConsumerService.cs
+++ LondonDataServices.IDecide.Core/Services/Foundations/Consumers/IConsumerService.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using LondonDataServices.IDecide.Core.Models.Foundations.Consumers;
 
@@ -10,6 +11,7 @@ namespace LondonDataServices.IDecide.Core.Services.Foundations.Consumers
     public interface IConsumerService
     {
         ValueTask<Consumer> AddConsumerAsync(Consumer consumer);
+        ValueTask<IQueryable<Consumer>> RetrieveAllConsumersAsync();
         ValueTask<Consumer> ModifyConsumerAsync(Consumer consumer);
     }
 }
