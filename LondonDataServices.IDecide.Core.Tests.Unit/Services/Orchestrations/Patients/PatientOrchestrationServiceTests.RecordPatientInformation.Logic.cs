227d5fcdeeeb78a6e738835d1a46da75b3d3bf0c
Joe Owens <joe.owens@answerdigital.com>
2025-08-21 11:05:55 +0100

    ShouldRecordPatientInformationAsyncWhenPatientFoundWithValidCodeAndGenerateNewCodeIsTrue -> PASS

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Patients/PatientOrchestrationServiceTests.RecordPatientInformation.Logic.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Patients/PatientOrchestrationServiceTests.RecordPatientInformation.Logic.cs
@@ -230,5 +230,113 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Pat
             this.patientServiceMock.VerifyNoOtherCalls();
             this.notificationServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldRecordPatientInformationAsyncWhenPatientFoundWithValidCodeAndGenerateNewCodeIsTrue()
+        {
+            // given
+            int expireAfterMinutes = this.patientOrchestrationConfigurations.ValidationCodeExpireAfterMinutes;
+            string randomNhsNumber = GenerateRandom10DigitNumber();
+            string inputNhsNumber = randomNhsNumber.DeepClone();
+            string randomCaptchaToken = GetRandomString();
+            string inputCaptchaToken = randomCaptchaToken.DeepClone();
+            NotificationPreference randomNotificationPreference = NotificationPreference.Email;
+            NotificationPreference inputNotificationPreference = randomNotificationPreference.DeepClone();
+            string notificationPreferenceString = inputNotificationPreference.ToString();
+            string outputValidationCode = GetRandomStringWithLengthOf(5);
+            DateTimeOffset randomDateTimeOffest = GetRandomDateTimeOffset();
+            DateTimeOffset outputDateTimeOffset = randomDateTimeOffest.DeepClone();
+            Patient randomPatient = GetRandomPatientWithNhsNumber(inputNhsNumber);
+            randomPatient.ValidationCodeExpiresOn = outputDateTimeOffset.AddMinutes(expireAfterMinutes);
+            randomPatient.NotificationPreference = inputNotificationPreference;
+            Patient outputPatient = randomPatient.DeepClone();
+            List<Patient> randomPatients = GetRandomPatients();
+            randomPatients.Add(outputPatient);
+            List<Patient> outputPatients = randomPatients.DeepClone();
+            Patient updatedPatient = outputPatient.DeepClone();
+            updatedPatient.ValidationCode = outputValidationCode;
+            updatedPatient.ValidationCodeExpiresOn = outputDateTimeOffset.AddMinutes(expireAfterMinutes);
+            updatedPatient.NotificationPreference = inputNotificationPreference;
+
+            NotificationInfo inputNotificationInfo = new NotificationInfo
+            {
+                Patient = updatedPatient
+            };
+
+            PatientOrchestrationConfigurations patientOrchestrationConfigurations =
+                new PatientOrchestrationConfigurations
+                {
+                    ValidationCodeExpireAfterMinutes = 1440
+                };
+
+            var patientOrchestrationServiceMock = new Mock<PatientOrchestrationService>(
+                loggingBrokerMock.Object,
+                securityBrokerMock.Object,
+                dateTimeBrokerMock.Object,
+                pdsServiceMock.Object,
+                patientServiceMock.Object,
+                notificationServiceMock.Object,
+                patientOrchestrationConfigurations)
+            { CallBase = true };
+
+            this.patientServiceMock.Setup(service =>
+                service.RetrieveAllPatientsAsync())
+                    .ReturnsAsync(randomPatients.AsQueryable);
+
+            this.pdsServiceMock.Setup(service =>
+                service.PatientLookupByNhsNumberAsync(inputNhsNumber))
+                    .ReturnsAsync(outputPatient);
+
+            patientOrchestrationServiceMock.Setup(service =>
+                service.GenerateValidationCode())
+                    .Returns(outputValidationCode);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(outputDateTimeOffset);
+
+            this.patientServiceMock.Setup(service =>
+                service.ModifyPatientAsync(It.Is(SamePatientAs(updatedPatient))))
+                    .ReturnsAsync(updatedPatient);
+
+            // when
+            await patientOrchestrationServiceMock.Object.RecordPatientInformation(
+                inputNhsNumber,
+                inputCaptchaToken,
+                notificationPreferenceString,
+                true);
+
+            //then
+            this.patientServiceMock.Verify(service =>
+                service.RetrieveAllPatientsAsync(),
+                    Times.Once);
+
+            this.pdsServiceMock.Verify(service =>
+                service.PatientLookupByNhsNumberAsync(inputNhsNumber),
+                    Times.Once);
+
+            patientOrchestrationServiceMock.Verify(service =>
+                service.GenerateValidationCode(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.patientServiceMock.Verify(service =>
+                service.ModifyPatientAsync(It.Is(SamePatientAs(updatedPatient))),
+                    Times.Once);
+
+            this.notificationServiceMock.Verify(service =>
+                service.SendCodeNotificationAsync(It.Is(SameNotificationInfoAs(inputNotificationInfo))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.pdsServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.notificationServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
