fede8de4f98943fe458082e1bc93bedb6816796a
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-22 10:54:05 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Consumers/ConsumerServiceTests.RetrieveById.Validations.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Consumers;
+using LondonDataServices.IDecide.Core.Models.Foundations.Consumers.Exceptions;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Consumers
+{
+    public partial class ConsumerServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidConsumerId = Guid.Empty;
+
+            var invalidConsumerException =
+                new InvalidConsumerException(
+                    message: "Invalid consumer. Please correct the errors and try again.");
+
+            invalidConsumerException.AddData(
+                key: nameof(Consumer.Id),
+                values: "Id is required");
+
+            var expectedConsumerValidationException =
+                new ConsumerValidationException(
+                    message: "Consumer validation errors occurred, please try again.",
+                    innerException: invalidConsumerException);
+
+            // when
+            ValueTask<Consumer> retrieveConsumerByIdTask =
+                this.consumerService.RetrieveConsumerByIdAsync(invalidConsumerId);
+
+            ConsumerValidationException actualConsumerValidationException =
+                await Assert.ThrowsAsync<ConsumerValidationException>(
+                    retrieveConsumerByIdTask.AsTask);
+
+            // then
+            actualConsumerValidationException.Should()
+                .BeEquivalentTo(expectedConsumerValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                    broker.LogErrorAsync(It.Is(SameExceptionAs(
+                        expectedConsumerValidationException))),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectConsumerByIdAsync(It.IsAny<Guid>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
