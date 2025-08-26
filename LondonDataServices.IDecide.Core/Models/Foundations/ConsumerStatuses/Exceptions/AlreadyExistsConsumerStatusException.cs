cdb331c55693bc42b2762fa070c63f1cd9683d76
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-26 16:26:13 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfConsumerStatusAlreadyExistsAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core/Models/Foundations/ConsumerStatuses/Exceptions/AlreadyExistsConsumerStatusException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions
+{
+    public class AlreadyExistsConsumerStatusException : Xeption
+    {
+        public AlreadyExistsConsumerStatusException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
