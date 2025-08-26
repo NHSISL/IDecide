be695e5dd9ebbe9910588f4f25cc92d0a5abf0b7
Joe Owens <joe.owens@answerdigital.com>
2025-08-26 11:11:02 +0100

    GenerateValidationCode logic test

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Patients/PatientOrchestrationServiceTests.GenerateValidationCode.Logic.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Patients
+{
+    public partial class PatientOrchestrationServiceTests
+    {
+        [Fact]
+        public void ShouldGenerateValidationCode()
+        {
+            //given
+            int validationCodeLength = 5;
+
+            // when
+            string actualValidationCode = this.patientOrchestrationService.GenerateValidationCode();
+
+            //then
+            actualValidationCode.Should().HaveLength(validationCodeLength);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.pdsServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.notificationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
