6667c4ffecadf7f9d03ef9c2fd4446a15c33381e
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-26 16:26:13 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfConsumerStatusAlreadyExistsAndLogItAsync -> FAIL

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.Exceptions.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.Exceptions.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------
 
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses;
 using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions;
@@ -53,6 +54,71 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Consum
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedConsumerStatusDependencyException))),
+                        Times.Once);
+
+            this.securityBrokerMock.Verify(broker =>
+                broker.GetCurrentUserAsync(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertConsumerStatusAsync(It.IsAny<ConsumerStatus>()),
+                    Times.Never);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfConsumerStatusAlreadyExistsAndLogItAsync()
+        {
+            // given
+            ConsumerStatus randomConsumerStatus = CreateRandomConsumerStatus();
+            ConsumerStatus alreadyExistsConsumerStatus = randomConsumerStatus;
+            string randomMessage = GetRandomString();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(randomMessage);
+
+            var alreadyExistsConsumerStatusException =
+                new AlreadyExistsConsumerStatusException(
+                    message: "ConsumerSstatus with the same Id already exists.",
+                    innerException: duplicateKeyException);
+
+            var expectedConsumerStatusDependencyValidationException =
+                new ConsumerStatusDependencyValidationException(
+                    message: "ConsumerStatus dependency validation occurred, please try again.",
+                    innerException: alreadyExistsConsumerStatusException);
+
+            this.securityAuditBrokerMock.Setup(broker =>
+                broker.ApplyAddAuditValuesAsync(It.IsAny<ConsumerStatus>()))
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<ConsumerStatus> addConsumerStatusTask =
+                this.consumerStatusService.AddConsumerStatusAsync(alreadyExistsConsumerStatus);
+
+            // then
+            ConsumerStatusDependencyValidationException actualConsumerStatusDependencyValidationException =
+                await Assert.ThrowsAsync<ConsumerStatusDependencyValidationException>(
+                    addConsumerStatusTask.AsTask);
+
+            actualConsumerStatusDependencyValidationException.Should()
+                .BeEquivalentTo(expectedConsumerStatusDependencyValidationException);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.ApplyAddAuditValuesAsync(It.IsAny<ConsumerStatus>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedConsumerStatusDependencyValidationException))),
                         Times.Once);
 
             this.securityBrokerMock.Verify(broker =>
---
+++ LondonDataServices.IDecide.Core/Models/Foundations/ConsumerStatuses/Exceptions/ConsumerStatusDependencyValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions
+{
+    internal class ConsumerStatusDependencyValidationException : Xeption
+    {
+        public ConsumerStatusDependencyValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
