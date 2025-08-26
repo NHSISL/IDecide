bc07f218e8c8885847574c0846021f6af1aaa121
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-26 14:54:54 +0100

    ShouldAddConsumerStatusAsync -> PASS

---
+++ LondonDataServices.IDecide.Core/Services/Foundations/ConsumerStatuses/ConsumerStatusService.Validations.cs
@@ -0,0 +1,75 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions;
+using LondonDataServices.IDecide.Core.Models.Securities;
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Services.Foundations.ConsumerStatuses
+{
+    public partial class ConsumerStatusService
+    {
+        private async ValueTask ValidateConsumerStatusOnAdd(ConsumerStatus consumerStatus)
+        {
+            User currentUser = await this.securityBroker.GetCurrentUserAsync();
+
+            Validate<InvalidConsumerStatusException>(
+                message: "Invalid consumer status. Please correct the errors and try again.",
+
+                (Rule: await IsNotRecentAsync(consumerStatus.CreatedDate),
+                    Parameter: nameof(ConsumerStatus.CreatedDate)));
+        }
+
+        private async ValueTask<dynamic> IsNotRecentAsync(DateTimeOffset date)
+        {
+            var (isNotRecent, startDate, endDate) = await IsDateNotRecentAsync(date);
+
+            return new
+            {
+                Condition = isNotRecent,
+                Message = $"Date is not recent. Expected a value between {startDate} and {endDate} but found {date}"
+            };
+        }
+
+        private async ValueTask<(bool IsNotRecent, DateTimeOffset StartDate, DateTimeOffset EndDate)>
+            IsDateNotRecentAsync(DateTimeOffset date)
+        {
+            int pastThreshold = 90;
+            int futureThreshold = 0;
+            DateTimeOffset currentDateTime = await this.dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+
+            if (currentDateTime == default)
+            {
+                return (false, default, default);
+            }
+
+            DateTimeOffset startDate = currentDateTime.AddSeconds(-pastThreshold);
+            DateTimeOffset endDate = currentDateTime.AddSeconds(futureThreshold);
+            bool isNotRecent = date < startDate || date > endDate;
+
+            return (isNotRecent, startDate, endDate);
+        }
+
+        private static void Validate<T>(string message, params (dynamic Rule, string Parameter)[] validations)
+            where T : Xeption
+        {
+            var invalidDataException = (T)Activator.CreateInstance(typeof(T), message);
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidDataException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidDataException.ThrowIfContainsErrors();
+        }
+    }
+}
