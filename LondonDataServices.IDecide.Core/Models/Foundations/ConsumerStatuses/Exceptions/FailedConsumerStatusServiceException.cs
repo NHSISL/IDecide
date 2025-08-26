938c2c76c8891e969705d0422caf7b25dc1c2f54
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-26 16:38:33 +0100

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core/Models/Foundations/ConsumerStatuses/Exceptions/FailedConsumerStatusServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions
+{
+    public class FailedConsumerStatusServiceException : Xeption
+    {
+        public FailedConsumerStatusServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
