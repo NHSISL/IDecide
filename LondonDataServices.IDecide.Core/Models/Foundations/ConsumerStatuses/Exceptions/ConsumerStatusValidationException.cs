d900c3c0f406c79a804c5c1ac8f71d502c9b8517
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-26 15:06:27 +0100

    ShouldThrowValidationExceptionOnAddIfConsumerStatusIsNullAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core/Models/Foundations/ConsumerStatuses/Exceptions/ConsumerStatusValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions
+{
+    public class ConsumerStatusValidationException : Xeption
+    {
+        public ConsumerStatusValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
