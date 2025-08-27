3512de7424de67ab7a710a6c8bcc4bab6d1012f0
Joe Owens <joe.owens@answerdigital.com>
2025-08-27 16:25:17 +0100

    ShouldGenerateValidationCodeAsync -> PASS

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/Patients/PatientServiceTests.GenerateValidationCodeAsync.Logic.cs
@@ -0,0 +1,32 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Patients
+{
+    public partial class PatientServiceTests
+    {
+        [Fact]
+        public async Task ShouldGenerateValidationCodeAsync()
+        {
+            //given
+            int validationCodeLength = 5;
+
+            // when
+            string actualValidationCode = await this.patientService.GenerateValidationCodeAsync();
+
+            // then
+            actualValidationCode.Should().HaveLength(validationCodeLength);
+            actualValidationCode.Should().MatchRegex("^[A-Z0-9]+$");
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
