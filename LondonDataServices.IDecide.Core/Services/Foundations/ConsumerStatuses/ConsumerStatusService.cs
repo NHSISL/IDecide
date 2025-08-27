59943d07d2a9095055d97e622fe9ccdf1d51a971
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-27 12:46:04 +0100

    ShouldModifyConsumerStatusAsync -> FAIL

--- LondonDataServices.IDecide.Core/Services/Foundations/ConsumerStatuses/ConsumerStatusService.cs
+++ LondonDataServices.IDecide.Core/Services/Foundations/ConsumerStatuses/ConsumerStatusService.cs
@@ -42,6 +42,9 @@ namespace LondonDataServices.IDecide.Core.Services.Foundations.ConsumerStatuses
                 return await this.storageBroker.InsertConsumerStatusAsync(consumerStatus);
             });
 
-
+        public ValueTask<ConsumerStatus> ModifyConsumerStatusAsync(ConsumerStatus consumerStatus)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
