1ab09a3903d9f0a784c2d56662f6feddb6595dfb
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-28 11:16:10 +0100

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync -> FAIL

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.RetrieveAll.Exceptions.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.RetrieveAll.Exceptions.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -53,6 +54,54 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Consum
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedConsumerStatusDependencyException))),
+                        Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string exceptionMessage = GetRandomString();
+            var serviceException = new Exception(exceptionMessage);
+
+            var failedConsumerStatusServiceException =
+                new FailedConsumerStatusServiceException(
+                    message: "Failed consumerStatus service occurred, please contact support",
+                    innerException: serviceException);
+
+            var expectedConsumerStatusServiceException =
+                new ConsumerStatusServiceException(
+                    message: "ConsumerStatus service error occurred, contact support.",
+                    innerException: failedConsumerStatusServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllConsumerStatusesAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<IQueryable<ConsumerStatus>> retrieveAllConsumerStatusesTask =
+                this.consumerStatusService.RetrieveAllConsumerStatusesAsync();
+
+            ConsumerStatusServiceException actualConsumerStatusServiceException =
+                await Assert.ThrowsAsync<ConsumerStatusServiceException>(retrieveAllConsumerStatusesTask.AsTask);
+
+            // then
+            actualConsumerStatusServiceException.Should()
+                .BeEquivalentTo(expectedConsumerStatusServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllConsumerStatusesAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedConsumerStatusServiceException))),
                         Times.Once);
 
             this.securityAuditBrokerMock.VerifyNoOtherCalls();
