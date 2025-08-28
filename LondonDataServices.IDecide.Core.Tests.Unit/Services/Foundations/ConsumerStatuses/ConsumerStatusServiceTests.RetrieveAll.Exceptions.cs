6f1c2f60f5b5071056b34ac97c13dc0bce8a2058
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-28 11:05:15 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.RetrieveAll.Exceptions.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.ConsumerStatuses
+{
+    public partial class ConsumerStatusServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedConsumerStatusStorageException =
+                new FailedConsumerStatusStorageException(
+                    message: "Failed consumerStatus storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedConsumerStatusDependencyException =
+                new ConsumerStatusDependencyException(
+                    message: "ConsumerStatus dependency error occurred, contact support.",
+                    innerException: failedConsumerStatusStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllConsumerStatusesAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<ConsumerStatus>> retrieveAllConsumerStatusesTask =
+                this.consumerStatusService.RetrieveAllConsumerStatusesAsync();
+
+            ConsumerStatusDependencyException actualConsumerStatusDependencyException =
+                await Assert.ThrowsAsync<ConsumerStatusDependencyException>(
+                    retrieveAllConsumerStatusesTask.AsTask);
+
+            // then
+            actualConsumerStatusDependencyException.Should()
+                .BeEquivalentTo(expectedConsumerStatusDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllConsumerStatusesAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedConsumerStatusDependencyException))),
+                        Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
