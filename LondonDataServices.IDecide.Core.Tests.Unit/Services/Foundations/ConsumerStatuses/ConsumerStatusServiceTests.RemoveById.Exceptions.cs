ead502a594cdb5d6a9ac9b123f815046ff221bd0
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-28 16:09:12 +0100

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.RemoveById.Exceptions.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/ConsumerStatuses/ConsumerStatusServiceTests.RemoveById.Exceptions.cs
@@ -2,11 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses;
 using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerStatuses.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.ConsumerStatuses
@@ -61,6 +63,61 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.Consum
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someConsumerStatusId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedConsumerStatusException =
+                new LockedConsumerStatusException(
+                    message: "Locked consumerStatus record exception, please try again later",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedConsumerStatusDependencyValidationException =
+                new ConsumerStatusDependencyValidationException(
+                    message: "ConsumerStatus dependency validation occurred, please try again.",
+                    innerException: lockedConsumerStatusException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectConsumerStatusByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<ConsumerStatus> removeConsumerStatusByIdTask =
+                this.consumerStatusService.RemoveConsumerStatusByIdAsync(someConsumerStatusId);
+
+            ConsumerStatusDependencyValidationException actualConsumerStatusDependencyValidationException =
+                await Assert.ThrowsAsync<ConsumerStatusDependencyValidationException>(
+                    removeConsumerStatusByIdTask.AsTask);
+
+            // then
+            actualConsumerStatusDependencyValidationException.Should()
+                .BeEquivalentTo(expectedConsumerStatusDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectConsumerStatusByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedConsumerStatusDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteConsumerStatusAsync(It.IsAny<ConsumerStatus>()),
                     Times.Never);
 
             this.securityAuditBrokerMock.VerifyNoOtherCalls();
