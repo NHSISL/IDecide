6742515d58e2faaeb534d5eca6f319d0176aaeae
Joe Owens <joe.owens@answerdigital.com>
2025-09-05 14:11:37 +0100

    CONTROLLERS: PatientCode

--- LondonDataServices.IDecide.Core/Services/Orchestrations/Patients/IPatientOrchestrationService.cs
+++ LondonDataServices.IDecide.Core/Services/Orchestrations/Patients/IPatientOrchestrationService.cs
@@ -16,5 +16,7 @@ namespace LondonDataServices.IDecide.Core.Services.Orchestrations.Patients
             string nhsNumber,
             string notificationPreference,
             bool generateNewCode = false);
+
+        ValueTask VerifyPatientCodeAsync(string nhsNumber, string verificationCode);
     }
 }
---
+++ LondonDataServices.IDecide.Portal.Server/Controllers/PatientCodeController.cs
@@ -0,0 +1,89 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Orchestrations.Patients.Exceptions;
+using LondonDataServices.IDecide.Core.Services.Orchestrations.Patients;
+using LondonDataServices.IDecide.Portal.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace LondonDataServices.IDecide.Portal.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientCodeController : RESTFulController
+    {
+        private readonly IPatientOrchestrationService patientOrchestrationService;
+
+        public PatientCodeController(IPatientOrchestrationService patientOrchestrationService)
+        {
+            this.patientOrchestrationService = patientOrchestrationService;
+        }
+
+        [HttpPost("PatientGenerationRequest")]
+        public async ValueTask<ActionResult> PostPatientGenerationRequestAsync(
+            [FromBody] PatientCodeRequest patientCodeRequest)
+        {
+            try
+            {
+                await this.patientOrchestrationService.RecordPatientInformationAsync(
+                    patientCodeRequest.NhsNumber,
+                    patientCodeRequest.NotificationPreference,
+                    patientCodeRequest.GenerateNewCode
+                );
+
+                return Ok();
+            }
+            catch (PatientOrchestrationValidationException patientOrchestrationValidationException)
+            {
+                return BadRequest(patientOrchestrationValidationException.InnerException);
+            }
+            catch (PatientOrchestrationDependencyValidationException
+                patientOrchestrationDependencyValidationException)
+            {
+                return BadRequest(patientOrchestrationDependencyValidationException.InnerException);
+            }
+            catch (PatientOrchestrationDependencyException patientOrchestrationDependencyException)
+            {
+                return InternalServerError(patientOrchestrationDependencyException);
+            }
+            catch (PatientOrchestrationServiceException patientOrchestrationServiceException)
+            {
+                return InternalServerError(patientOrchestrationServiceException);
+            }
+        }
+
+        [HttpPost("VerifyPatientCode")]
+        public async ValueTask<ActionResult> VerifyPatientCodeAsync([FromBody] PatientCodeRequest patientCodeRequest)
+        {
+            try
+            {
+                await this.patientOrchestrationService.VerifyPatientCodeAsync(
+                    patientCodeRequest.NhsNumber,
+                    patientCodeRequest.VeriicationCode
+                );
+
+                return Ok();
+            }
+            catch (PatientOrchestrationValidationException patientOrchestrationValidationException)
+            {
+                return BadRequest(patientOrchestrationValidationException.InnerException);
+            }
+            catch (PatientOrchestrationDependencyValidationException
+                patientOrchestrationDependencyValidationException)
+            {
+                return BadRequest(patientOrchestrationDependencyValidationException.InnerException);
+            }
+            catch (PatientOrchestrationDependencyException patientOrchestrationDependencyException)
+            {
+                return InternalServerError(patientOrchestrationDependencyException);
+            }
+            catch (PatientOrchestrationServiceException patientOrchestrationServiceException)
+            {
+                return InternalServerError(patientOrchestrationServiceException);
+            }
+        }
+    }
+}
