5176e4c7c60f172e0ce7cff0fb721da879c79d3a
davidhayes03 <david.hayes17@nhs.net>
2025-09-19 15:09:54 +0100

    CODE RUB: Lint any type

--- LondonDataServices.IDecide.Portal.Client/src/components/confirmCode/confirmCode.tsx
+++ LondonDataServices.IDecide.Portal.Client/src/components/confirmCode/confirmCode.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Patient } from "../../models/patients/patient";
