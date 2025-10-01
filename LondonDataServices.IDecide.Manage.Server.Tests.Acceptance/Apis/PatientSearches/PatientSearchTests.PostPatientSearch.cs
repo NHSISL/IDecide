73ae1291933e149688cafaeaeae9a4bfe73d57aa
Joe Owens <joe.owens@answerdigital.com>
2025-10-01 10:10:56 +0100

    ACCEPTANCE: PatientSearch - PostPatientSearch Management

---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Apis/PatientSearches/PatientSearchTests.PostPatientSearch.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LondonDataServices.IDecide.Core.Models.Foundations.Pds;
+using LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.Patients;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Apis.PatientSearches
+{
+    public partial class PatientSearchTests
+    {
+        [Fact]
+        public async Task ShouldPostPatientSearch()
+        {
+            // given
+            string inputSurname = "Smith";
+            PatientLookup randomPatientLookup = GetRandomSearchPatientLookup(inputSurname);
+            PatientLookup inputPatientLookup = randomPatientLookup.DeepClone();
+            Patient expectedPatient = GetPatient(inputSurname);
+
+            // when
+            Patient actualPatient =
+                await this.apiBroker.PostPatientSearchAsync(inputPatientLookup);
+
+            // then
+            actualPatient.Should().BeEquivalentTo(expectedPatient);
+        }
+    }
+}
--- LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/TestWebApplicationFactory.cs
+++ LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/TestWebApplicationFactory.cs
@@ -7,6 +7,9 @@ using System.IO;
 using System.Linq;
 using Attrify.InvisibleApi.Models;
 using ISL.Providers.Notifications.Abstractions;
+using ISL.Providers.PDS.Abstractions;
+using ISL.Providers.PDS.FakeFHIR.Models;
+using ISL.Providers.PDS.FakeFHIR.Providers.FakeFHIR;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -35,6 +38,7 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
             builder.ConfigureServices((context, services) =>
             {
                 OverrideSecurityForTesting(services);
+                OverrideFhirProviderForTesting(services, context.Configuration);
                 OverrideNotificationProviderForTesting(services, context.Configuration);
             });
         }
@@ -78,6 +82,26 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
             {
                 options.AddPolicy("TestPolicy", policy => policy.RequireAssertion(_ => true));
             });
+        }
+
+        private static void OverrideFhirProviderForTesting(
+            IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var fhirDescriptor = services
+                .FirstOrDefault(d => d.ServiceType == typeof(IPdsProvider));
+
+            if (fhirDescriptor != null)
+            {
+                services.Remove(fhirDescriptor);
+            }
+
+            FakeFHIRProviderConfigurations fakeFHIRProviderConfigurations = configuration
+                 .GetSection("FakeFHIRProviderConfigurations")
+                     .Get<FakeFHIRProviderConfigurations>();
+
+            services.AddSingleton(fakeFHIRProviderConfigurations);
+            services.AddTransient<IPdsProvider, FakeFHIRProvider>();
         }
 
         private static void OverrideNotificationProviderForTesting(
