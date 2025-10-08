47b1cb63f941ef8487e3b2fd43aa46d152335516
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-08 16:34:15 +0100

    ShouldThrowServiceExceptionOnAdoptPatientDecisionsIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core/Models/Orchestrations/Consumers/Exceptions/ConsumerOrchestrationServiceException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Orchestrations.Consumers.Exceptions
+{
+    public class ConsumerOrchestrationServiceException : Xeption
+    {
+        public ConsumerOrchestrationServiceException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ LondonDataServices.IDecide.Core/Models/Orchestrations/Consumers/Exceptions/FailedConsumerOrchestrationServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Models.Orchestrations.Consumers.Exceptions
+{
+    public class FailedConsumerOrchestrationServiceException : Xeption
+    {
+        public FailedConsumerOrchestrationServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
