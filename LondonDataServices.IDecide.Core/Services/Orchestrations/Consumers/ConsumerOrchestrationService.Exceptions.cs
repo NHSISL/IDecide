71c31b82f8df0470ee8d6690e5930caadf00b670
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-08 15:25:45 +0100

    ShouldThrowInvalidDecisionsExceptionOnAdoptPatientDecisionsIfDecisionsIsEmptyAndLogItAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core/Services/Orchestrations/Consumers/ConsumerOrchestrationService.Exceptions.cs
@@ -0,0 +1,41 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Orchestrations.Consumers.Exceptions;
+using Xeptions;
+
+namespace LondonDataServices.IDecide.Core.Services.Orchestrations.Consumers
+{
+    public partial class ConsumerOrchestrationService
+    {
+        private delegate ValueTask ReturningNothingFunction();
+
+        private async ValueTask TryCatch(ReturningNothingFunction returningNothingFunction)
+        {
+            try
+            {
+                await returningNothingFunction();
+            }
+            catch (InvalidDecisionsException invalidDecisionsException)
+            {
+                throw await CreateAndLogValidationExceptionAsync(invalidDecisionsException);
+            }
+        }
+
+        private async ValueTask<ConsumerOrchestrationValidationException>
+            CreateAndLogValidationExceptionAsync(Xeption exception)
+        {
+            var consumerOrchestrationValidationException =
+                new ConsumerOrchestrationValidationException(
+                    message: "Consumer orchestration validation error occurred, " +
+                             "please fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(consumerOrchestrationValidationException);
+
+            return consumerOrchestrationValidationException;
+        }
+    }
+}
