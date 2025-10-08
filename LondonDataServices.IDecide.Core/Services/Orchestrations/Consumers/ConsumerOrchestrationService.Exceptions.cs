8e6d3dd0c7145cf42001ed474207436a2d947234
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-08 16:12:53 +0100

    ShouldThrowDependencyExceptionOnAdoptPatientDecisionsAndLogItAsync -> FAIL

--- LondonDataServices.IDecide.Core/Services/Orchestrations/Consumers/ConsumerOrchestrationService.Exceptions.cs
+++ LondonDataServices.IDecide.Core/Services/Orchestrations/Consumers/ConsumerOrchestrationService.Exceptions.cs
@@ -3,6 +3,10 @@
 // ---------------------------------------------------------
 
 using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerAdoptions.Exceptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Consumers.Exceptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications.Exceptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Patients.Exceptions;
 using LondonDataServices.IDecide.Core.Models.Orchestrations.Consumers.Exceptions;
 using Xeptions;
 
@@ -22,6 +26,39 @@ namespace LondonDataServices.IDecide.Core.Services.Orchestrations.Consumers
             {
                 throw await CreateAndLogValidationExceptionAsync(invalidDecisionsException);
             }
+            catch (ConsumerValidationException consumerValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(consumerValidationException);
+            }
+            catch (ConsumerDependencyValidationException consumerDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(consumerDependencyValidationException);
+            }
+            catch (ConsumerAdoptionValidationException consumerAdoptionValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(consumerAdoptionValidationException);
+            }
+            catch (ConsumerAdoptionDependencyValidationException consumerAdoptionDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    consumerAdoptionDependencyValidationException);
+            }
+            catch (PatientValidationException patientValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(patientValidationException);
+            }
+            catch (PatientDependencyValidationException patientDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(patientDependencyValidationException);
+            }
+            catch (NotificationValidationException notificationValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(notificationValidationException);
+            }
+            catch (NotificationDependencyValidationException notificationDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(notificationDependencyValidationException);
+            }
         }
 
         private async ValueTask<ConsumerOrchestrationValidationException>
@@ -36,6 +73,20 @@ namespace LondonDataServices.IDecide.Core.Services.Orchestrations.Consumers
             await this.loggingBroker.LogErrorAsync(consumerOrchestrationValidationException);
 
             return consumerOrchestrationValidationException;
+        }
+
+        private async ValueTask<ConsumerOrchestrationDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var consumerOrchestrationDependencyValidationException =
+                new ConsumerOrchestrationDependencyValidationException(
+                    message: "Consumer orchestration dependency validation error occurred, " +
+                             "please fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(consumerOrchestrationDependencyValidationException);
+
+            return consumerOrchestrationDependencyValidationException;
         }
     }
 }
