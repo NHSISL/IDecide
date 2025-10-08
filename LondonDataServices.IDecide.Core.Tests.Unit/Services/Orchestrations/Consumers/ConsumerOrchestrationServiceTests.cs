1496fb450e9e6856a519fea51b6c4bdc190f10e3
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-08 13:21:33 +0100

    ShouldAdoptPatientDecisionsAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Consumers/ConsumerOrchestrationServiceTests.cs
@@ -0,0 +1,191 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using LondonDataServices.IDecide.Core.Brokers.DateTimes;
+using LondonDataServices.IDecide.Core.Brokers.Loggings;
+using LondonDataServices.IDecide.Core.Brokers.Securities;
+using LondonDataServices.IDecide.Core.Models.Foundations.Consumers;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Patients;
+using LondonDataServices.IDecide.Core.Models.Securities;
+using LondonDataServices.IDecide.Core.Services.Foundations.ConsumerAdoptions;
+using LondonDataServices.IDecide.Core.Services.Foundations.Consumers;
+using LondonDataServices.IDecide.Core.Services.Foundations.Notifications;
+using LondonDataServices.IDecide.Core.Services.Foundations.Patients;
+using LondonDataServices.IDecide.Core.Services.Orchestrations.Consumers;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Consumers
+{
+    public partial class ConsumerOrchestrationServiceTests
+    {
+        private readonly Mock<ILoggingBroker> loggingBrokerMock = new Mock<ILoggingBroker>();
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+        private readonly Mock<ISecurityBroker> securityBrokerMock = new Mock<ISecurityBroker>();
+        private readonly Mock<IConsumerService> consumerServiceMock = new Mock<IConsumerService>();
+
+        private readonly Mock<IConsumerAdoptionService> consumerAdoptionServiceMock =
+            new Mock<IConsumerAdoptionService>();
+
+        private readonly Mock<INotificationService> notificationServiceMock = new Mock<INotificationService>();
+        private readonly Mock<IPatientService> patientServiceMock = new Mock<IPatientService>();
+        private readonly IConsumerOrchestrationService consumerOrchestrationService;
+
+        public ConsumerOrchestrationServiceTests()
+        {
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.securityBrokerMock = new Mock<ISecurityBroker>();
+            this.consumerServiceMock = new Mock<IConsumerService>();
+            this.consumerAdoptionServiceMock = new Mock<IConsumerAdoptionService>();
+            this.notificationServiceMock = new Mock<INotificationService>();
+            this.patientServiceMock = new Mock<IPatientService>();
+
+
+            this.consumerOrchestrationService = new ConsumerOrchestrationService(
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                securityBroker: this.securityBrokerMock.Object,
+                consumerService: this.consumerServiceMock.Object,
+                consumerAdoptionService: this.consumerAdoptionServiceMock.Object,
+                notificationService: this.notificationServiceMock.Object,
+                patientService: this.patientServiceMock.Object);
+        }
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
+
+        private static List<Claim> CreateRandomClaims()
+        {
+            string randomString = GetRandomString();
+
+            return Enumerable.Range(start: 1, count: GetRandomNumber())
+                .Select(_ => new Claim(type: randomString, value: randomString)).ToList();
+        }
+
+        private static User CreateRandomUser()
+        {
+            string randomString = GetRandomString();
+            Guid randomGuid = Guid.NewGuid();
+
+            User user = new User(
+                userId: randomGuid.ToString(),
+                givenName: randomString,
+                surname: randomString,
+                displayName: randomString,
+                email: randomString,
+                jobTitle: randomString,
+                roles: new List<string> { randomString },
+                claims: CreateRandomClaims());
+
+            return user;
+        }
+
+        private static List<Decision> CreateRandomDecisions()
+        {
+            return CreateDecisionFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                .AsQueryable()
+                .ToList();
+        }
+
+        private static List<Decision> CreateRandomDecisionsWithoutPatient()
+        {
+            var decisions = CreateDecisionFiller(GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                .ToList();
+
+            foreach (var decision in decisions)
+            {
+                decision.Patient = null;
+            }
+
+            return decisions;
+        }
+
+        private static Filler<Decision> CreateDecisionFiller(DateTimeOffset dateTimeOffset, string userId = "")
+        {
+            userId = string.IsNullOrEmpty(userId) ? Guid.NewGuid().ToString() : userId;
+            var filler = new Filler<Decision>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+
+                .OnProperty(decision => decision.DecisionChoice).Use(GetRandomStringWithLengthOf(255))
+                .OnProperty(decision => decision.CreatedBy).Use(userId)
+                .OnProperty(decision => decision.UpdatedBy).Use(userId)
+                .OnProperty(decision => decision.DecisionType).IgnoreIt()
+                .OnProperty(decision => decision.ConsumerAdoptions).IgnoreIt();
+
+            return filler;
+        }
+
+        private static Patient GetRandomPatient(
+            DateTimeOffset validationCodeExpiresOn,
+            string inputNhsNumber = "1234567890",
+            string validationCode = "A1B2C",
+            int retryCount = 0) =>
+            CreatePatientFiller(validationCodeExpiresOn, inputNhsNumber, validationCode, retryCount).Create();
+
+        private static List<Patient> GetRandomPatients(DateTimeOffset validationCodeExpiresOn) =>
+            CreatePatientFiller(validationCodeExpiresOn).Create(GetRandomNumber()).ToList();
+
+        private static Consumer CreateRandomConsumer() =>
+            CreateConsumerFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
+
+        private static Filler<Consumer> CreateConsumerFiller(DateTimeOffset dateTimeOffset, string userId = "")
+        {
+            userId = string.IsNullOrEmpty(userId) ? Guid.NewGuid().ToString() : userId;
+            var filler = new Filler<Consumer>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnProperty(consumer => consumer.Name).Use(GetRandomStringWithLengthOf(255))
+                .OnProperty(consumer => consumer.Name).Use(GetRandomStringWithLengthOf(36))
+                .OnProperty(consumer => consumer.CreatedBy).Use(userId)
+                .OnProperty(consumer => consumer.UpdatedBy).Use(userId)
+                .OnProperty(consumer => consumer.ConsumerAdoptions).IgnoreIt();
+
+            return filler;
+        }
+
+        private static Filler<Patient> CreatePatientFiller(
+            DateTimeOffset validationCodeExpiresOn,
+            string inputNhsNumber = "1234567890",
+            string validationCode = "A1B2C",
+            int retryCount = 0)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            var filler = new Filler<Patient>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(patient => patient.ValidationCodeExpiresOn).Use(validationCodeExpiresOn)
+                .OnProperty(patient => patient.NhsNumber).Use(inputNhsNumber)
+                .OnProperty(patient => patient.ValidationCode).Use(validationCode)
+                .OnProperty(patient => patient.RetryCount).Use(retryCount);
+
+            return filler;
+        }
+    }
+}
