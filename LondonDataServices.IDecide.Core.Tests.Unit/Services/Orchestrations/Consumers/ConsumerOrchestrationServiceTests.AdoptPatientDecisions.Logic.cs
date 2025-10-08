91f1cefbe853b99649b0a08f92520b5a80124f31
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-08 13:21:33 +0100

    ShouldAdoptPatientDecisionsAsync -> FAIL

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Consumers/ConsumerOrchestrationServiceTests.AdoptPatientDecisions.Logic.cs
@@ -0,0 +1,109 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerAdoptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Consumers;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications;
+using LondonDataServices.IDecide.Core.Models.Securities;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Consumers
+{
+    public partial class ConsumerOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldAdoptPatientDecisionsAsync()
+        {
+            // given
+            List<Decision> randomDecisions = CreateRandomDecisions();
+            List<Decision> inputDecisions = randomDecisions;
+            User randomUser = CreateRandomUser();
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            Consumer randomConsumer = CreateRandomConsumer();
+            Guid consumerId = Guid.Parse(randomUser.UserId);
+            randomConsumer.Id = consumerId;
+            List<ConsumerAdoption> consumerAdoptions = new List<ConsumerAdoption>();
+
+            foreach (var decision in inputDecisions)
+            {
+                consumerAdoptions.Add(new ConsumerAdoption
+                {
+                    ConsumerId = consumerId,
+                    DecisionId = decision.Id,
+                    AdoptionDate = randomDateTimeOffset
+                });
+            }
+
+            this.securityBrokerMock.Setup(broker =>
+                broker.GetCurrentUserAsync())
+                    .ReturnsAsync(randomUser);
+
+            this.consumerServiceMock.Setup(service =>
+                service.RetrieveConsumerByIdAsync(consumerId))
+                    .ReturnsAsync(randomConsumer);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            this.consumerAdoptionServiceMock.Setup(service =>
+                service.BulkAddOrModifyConsumerAdoptionsAsync(consumerAdoptions, It.IsAny<int>()))
+                    .Returns(ValueTask.CompletedTask);
+
+            foreach (var decision in inputDecisions)
+            {
+                this.notificationServiceMock.Setup(service =>
+                    service.SendSubscriberUsageNotificationAsync(
+                    It.Is<NotificationInfo>(info =>
+                        info.Decision == decision &&
+                        info.Patient == decision.Patient)))
+                    .Returns(ValueTask.CompletedTask);
+            }
+
+            // when
+            await this.consumerOrchestrationService.AdoptPatientDecisions(inputDecisions);
+
+            // then
+            this.securityBrokerMock.Verify(broker =>
+                broker.GetCurrentUserAsync(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Exactly(inputDecisions.Count));
+
+            this.consumerAdoptionServiceMock.Verify(service =>
+                service.BulkAddOrModifyConsumerAdoptionsAsync(
+                    It.Is<List<ConsumerAdoption>>(adoptions =>
+                        adoptions.Count == consumerAdoptions.Count &&
+                        Enumerable.Range(0, consumerAdoptions.Count).All(i =>
+                            adoptions[i].ConsumerId == consumerAdoptions[i].ConsumerId &&
+                            adoptions[i].DecisionId == consumerAdoptions[i].DecisionId &&
+                            adoptions[i].AdoptionDate == consumerAdoptions[i].AdoptionDate)),
+                    It.IsAny<int>()),
+                    Times.Once);
+
+            foreach (var decision in inputDecisions)
+            {
+                this.notificationServiceMock.Verify(service =>
+                    service.SendSubscriberUsageNotificationAsync(
+                        It.Is<NotificationInfo>(info =>
+                            info.Decision == decision &&
+                            info.Patient == decision.Patient)),
+                        Times.Once);
+            }
+
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.consumerAdoptionServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.notificationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LondonDataServices.IDecide.Core/Services/Orchestrations/Consumers/ConsumerOrchestrationService.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Brokers.DateTimes;
+using LondonDataServices.IDecide.Core.Brokers.Loggings;
+using LondonDataServices.IDecide.Core.Brokers.Securities;
+using LondonDataServices.IDecide.Core.Models.Foundations.ConsumerAdoptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications;
+using LondonDataServices.IDecide.Core.Services.Foundations.ConsumerAdoptions;
+using LondonDataServices.IDecide.Core.Services.Foundations.Consumers;
+using LondonDataServices.IDecide.Core.Services.Foundations.Notifications;
+using LondonDataServices.IDecide.Core.Services.Foundations.Patients;
+
+namespace LondonDataServices.IDecide.Core.Services.Orchestrations.Consumers
+{
+    public class ConsumerOrchestrationService : IConsumerOrchestrationService
+    {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ISecurityBroker securityBroker;
+        private readonly IConsumerService consumerService;
+        private readonly IConsumerAdoptionService consumerAdoptionService;
+        private readonly INotificationService notificationService;
+        private readonly IPatientService patientService;
+
+        public ConsumerOrchestrationService(
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker,
+            ISecurityBroker securityBroker,
+            IConsumerService consumerService,
+            IConsumerAdoptionService consumerAdoptionService,
+            INotificationService notificationService,
+            IPatientService patientService)
+        {
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.securityBroker = securityBroker;
+            this.consumerService = consumerService;
+            this.consumerAdoptionService = consumerAdoptionService;
+            this.notificationService = notificationService;
+            this.patientService = patientService;
+        }
+
+        public async ValueTask AdoptPatientDecisions(List<Decision> decisions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
