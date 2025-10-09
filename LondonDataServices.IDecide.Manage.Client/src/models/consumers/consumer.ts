8262b7ba8ca09dc90d4898dd0507b04bdfde0f1d
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-09 12:06:52 +0100

    DATA: Consumer

--- LondonDataServices.IDecide.Manage.Client/src/models/consumers/consumer.ts
+++ LondonDataServices.IDecide.Manage.Client/src/models/consumers/consumer.ts
@@ -2,8 +2,11 @@ import { ConsumerAdoption } from '../consumerAdoptions/consumerAdoption';
 
 export class Consumer {
     public id?: string;
+    public entraId?: string;
     public name?: string;
-    public accessToken?: string;
+    public contactPerson?: string;
+    public contactNumber?: string;
+    public contactEmail?: string;
     public createdBy?: string;
     public createdDate?: Date;
     public updatedBy?: string;
@@ -12,8 +15,11 @@ export class Consumer {
 
     constructor(consumer?: Partial<Consumer>) {
         this.id = consumer?.id || '';
+        this.entraId = consumer?.entraId || '';
         this.name = consumer?.name || '';
-        this.accessToken = consumer?.accessToken || '';
+        this.contactPerson = consumer?.contactPerson || '';
+        this.contactNumber = consumer?.contactNumber || '';
+        this.contactEmail = consumer?.contactEmail || '';
         this.createdBy = consumer?.createdBy || '';
         this.createdDate = consumer?.createdDate ? new Date(consumer.createdDate) : undefined;
         this.updatedBy = consumer?.updatedBy || '';
