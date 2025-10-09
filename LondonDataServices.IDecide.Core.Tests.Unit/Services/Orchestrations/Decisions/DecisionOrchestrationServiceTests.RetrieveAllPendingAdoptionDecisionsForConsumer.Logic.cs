59382e5f81c2b23586f27b9dfcf3e68045e5b9fe
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-09 15:50:16 +0100

    ShouldRetrieveAllPendingAdoptionDecisionsForConsumerForPopulatedChangesSinceDateAndDecisionTypeAsync -> PASS

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Decisions/DecisionOrchestrationServiceTests.RetrieveAllPendingAdoptionDecisionsForConsumer.Logic.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Decisions/DecisionOrchestrationServiceTests.RetrieveAllPendingAdoptionDecisionsForConsumer.Logic.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using LondonDataServices.IDecide.Core.Models.Foundations.Consumers;
 using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Foundations.DecisionTypes;
 using LondonDataServices.IDecide.Core.Models.Securities;
 using Moq;
 
@@ -70,8 +71,8 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Dec
             ShouldRetrieveAllPendingAdoptionDecisionsForConsumerForPopulatedChangesSinceDateAsync()
         {
             // given
-            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
-            DateTimeOffset changesSinceDate = randomDateTimeOffset.AddMinutes(-5);
+            DateTimeOffset nowDateTimeOffset = DateTimeOffset.Now;
+            DateTimeOffset changesSinceDate = nowDateTimeOffset.AddMinutes(-5);
             string decisionType = null;
             User randomUser = CreateRandomUser();
             IQueryable<Consumer> consumers = CreateRandomConsumersWithMatchingEntraIdEntry(randomUser.UserId);
@@ -93,6 +94,103 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Dec
             this.decisionServiceMock.Setup(service =>
                 service.RetrieveAllDecisionsAsync())
                     .ReturnsAsync(allDecisions.AsQueryable());
+
+            // when
+            List<Decision> actualPendingAdoptionDecisions =
+                await this.decisionOrchestrationService.RetrieveAllPendingAdoptionDecisionsForConsumer(
+                    changesSinceDate, decisionType);
+
+            // then
+            actualPendingAdoptionDecisions.Should()
+                .BeEquivalentTo(expectedPendingAdoptionDecisions);
+
+            this.securityBrokerMock.Verify(broker =>
+                broker.GetCurrentUserAsync(),
+                    Times.Once);
+
+            this.consumerServiceMock.Verify(service =>
+                service.RetrieveAllConsumersAsync(),
+                    Times.Once);
+
+            this.decisionServiceMock.Verify(service =>
+                service.RetrieveAllDecisionsAsync(),
+                    Times.Once);
+
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.consumerServiceMock.VerifyNoOtherCalls();
+            this.decisionServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldRetrieveAllPendingAdoptionDecisionsForConsumerForPopulatedChangesSinceDateAndDecisionTypeAsync()
+        {
+            // given
+            DateTimeOffset nowDateTimeOffset = DateTimeOffset.Now;
+            DateTimeOffset changesSinceDate = nowDateTimeOffset.AddMinutes(-5);
+            string decisionType = GetRandomString();
+            User randomUser = CreateRandomUser();
+            IQueryable<Consumer> consumers = CreateRandomConsumersWithMatchingEntraIdEntry(randomUser.UserId);
+            List<Decision> decisions = CreateRandomDecisions().ToList();
+
+            Decision excludedDecisionByDate = new Decision();
+
+            excludedDecisionByDate.DecisionType = new DecisionType
+            {
+                Id = Guid.NewGuid(),
+                Name = decisionType,
+                CreatedBy = decisions.First().CreatedBy,
+                UpdatedBy = decisions.First().UpdatedBy,
+                UpdatedDate = decisions.First().UpdatedDate
+            };
+
+            excludedDecisionByDate.CreatedDate = changesSinceDate.AddMinutes(-10);
+            decisions.Add(excludedDecisionByDate);
+            Decision excludedDecisionByDecisionType = new Decision();
+
+            excludedDecisionByDecisionType.DecisionType = new DecisionType
+            {
+                Id = Guid.NewGuid(),
+                Name = GetRandomString(),
+                CreatedBy = decisions.First().CreatedBy,
+                UpdatedBy = decisions.First().UpdatedBy,
+                UpdatedDate = decisions.First().UpdatedDate
+            };
+
+            excludedDecisionByDecisionType.CreatedDate = changesSinceDate.AddMinutes(2);
+            decisions.Add(excludedDecisionByDecisionType);
+            Decision includedDecision = new Decision();
+
+            includedDecision.DecisionType = new DecisionType
+            {
+                Id = Guid.NewGuid(),
+                Name = decisionType,
+                CreatedBy = decisions.First().CreatedBy,
+                UpdatedBy = decisions.First().UpdatedBy,
+                UpdatedDate = decisions.First().UpdatedDate
+            };
+
+            includedDecision.CreatedDate = changesSinceDate.AddMinutes(1);
+            decisions.Add(includedDecision);
+
+            List<Decision> expectedPendingAdoptionDecisions =
+                decisions
+                    .Where(d =>
+                        d.CreatedDate > changesSinceDate &&
+                        d.DecisionType?.Name == decisionType)
+                    .ToList();
+
+            this.securityBrokerMock.Setup(broker =>
+                broker.GetCurrentUserAsync())
+                    .ReturnsAsync(randomUser);
+
+            this.consumerServiceMock.Setup(service =>
+                service.RetrieveAllConsumersAsync())
+                    .ReturnsAsync(consumers);
+
+            this.decisionServiceMock.Setup(service =>
+                service.RetrieveAllDecisionsAsync())
+                    .ReturnsAsync(decisions.AsQueryable());
 
             // when
             List<Decision> actualPendingAdoptionDecisions =
