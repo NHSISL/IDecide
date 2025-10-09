3715e8eb30459481253bf3d3825d96ed3970c625
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-09 15:16:40 +0100

    ShouldRetrieveAllPendingAdoptionDecisionsForConsumerForDefaultChangesSinceDateAndDecisionTypeParamsAsync -> FAIL

--- LondonDataServices.IDecide.Core/Services/Orchestrations/Decisions/IDecisionOrchestrationService.cs
+++ LondonDataServices.IDecide.Core/Services/Orchestrations/Decisions/IDecisionOrchestrationService.cs
@@ -2,6 +2,8 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
 
@@ -10,5 +12,9 @@ namespace LondonDataServices.IDecide.Core.Services.Orchestrations.Decisions
     public interface IDecisionOrchestrationService
     {
         ValueTask VerifyAndRecordDecisionAsync(Decision decision);
+
+        ValueTask<List<Decision>> RetrieveAllPendingAdoptionDecisionsForConsumer(
+            DateTimeOffset changesSinceDate,
+            string decisionType);
     }
 }
