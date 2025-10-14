f0bbf4d82919cb43f07e1139187b1a9a07935c22
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-14 14:19:17 +0100

    PostShouldNotHaveInvisibleApiAttribute -> PASS

--- LondonDataServices.IDecide.Manage.Server.Tests.Unit/Controllers/ConsumerStatuses/ConsumerStatusControllerTests.AdoptPatientDecisions.Security.cs
+++ LondonDataServices.IDecide.Manage.Server.Tests.Unit/Controllers/ConsumerStatuses/ConsumerStatusControllerTests.AdoptPatientDecisions.Security.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Attrify.Attributes;
 using FluentAssertions;
 using LondonDataServices.IDecide.Manage.Server.Controllers.ConsumerStatuses;
 using Microsoft.AspNetCore.Authorization;
@@ -55,5 +56,27 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Unit.Controllers.Consum
             actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
         }
 
+        [Fact]
+        public void PostShouldNotHaveInvisibleApiAttribute()
+        {
+            // Given
+            var controllerType = typeof(ConsumerStatusController);
+            var methodInfo = controllerType.GetMethod("AdoptPatientDecisionsAsync");
+            Type attributeType = typeof(InvisibleApiAttribute);
+
+            // When
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // Then
+            attribute.Should().BeNull();
+        }
     }
 }
