74cb10f9ba2577aee2bef3b5279e81f27aed6c79
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-14 14:03:29 +0100

    ShouldReturnOkOnAdoptPatientDecisionsAsync -> FAIL

---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Unit/Controllers/ConsumerStatuses/ConsumerStatusControllerTests.AdoptPatientDecisions.Logic.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Task = System.Threading.Tasks.Task;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Unit.Controllers.ConsumerStatuses
+{
+    public partial class ConsumerStatusControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnOkOnAdoptPatientDecisionsAsync()
+        {
+            // given
+            List<Decision> randomDecisions = GetRandomDecisions();
+            List<Decision> inputDecisions = randomDecisions.DeepClone();
+            var expectedResult = new OkResult();
+            var expectedActionResult = expectedResult;
+
+            this.consumerOrchestrationServiceMock.Setup(orchestration =>
+                orchestration.AdoptPatientDecisions(inputDecisions))
+                    .Returns(ValueTask.CompletedTask);
+
+            // when
+            ActionResult actualActionResult =
+                await this.consumerStatusController.AdoptPatientDecisionsAsync(inputDecisions);
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.consumerOrchestrationServiceMock.Verify(orchestration =>
+                orchestration.AdoptPatientDecisions(inputDecisions),
+                    Times.Once);
+
+            this.consumerOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LondonDataServices.IDecide.Manage.Server/Controllers/ConsumerStatuses/ConsumerStatusController.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
+using LondonDataServices.IDecide.Core.Models.Orchestrations.Consumers.Exceptions;
+using LondonDataServices.IDecide.Core.Services.Orchestrations.Consumers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace LondonDataServices.IDecide.Manage.Server.Controllers.ConsumerStatuses
+{
+    [Authorize(Roles = "LondonDataServices.IDecide.Manage.Server.Administrators,LondonDataServices.IDecide.Manage.Server.Agents")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConsumerStatusController : RESTFulController
+    {
+        private readonly IConsumerOrchestrationService consumerOrchestrationService;
+
+        public ConsumerStatusController(IConsumerOrchestrationService consumerOrchestrationService) =>
+            this.consumerOrchestrationService = consumerOrchestrationService;
+
+        [HttpPost("AdoptPatientDecisions")]
+        public async ValueTask<ActionResult> AdoptPatientDecisionsAsync([FromBody] List<Decision> decisions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
