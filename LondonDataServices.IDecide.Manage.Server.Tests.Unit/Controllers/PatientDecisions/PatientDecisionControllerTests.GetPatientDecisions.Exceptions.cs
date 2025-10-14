4680fc9a1d28e463a8d15e50b42ca5f321a2a348
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-14 10:58:35 +0100

    ShouldReturnInternalServerErrorOnGetPatientDecisionsIfServerErrorOccurredAsync -> PASS

--- LondonDataServices.IDecide.Manage.Server.Tests.Unit/Controllers/PatientDecisions/PatientDecisionControllerTests.GetPatientDecisions.Exceptions.cs
+++ LondonDataServices.IDecide.Manage.Server.Tests.Unit/Controllers/PatientDecisions/PatientDecisionControllerTests.GetPatientDecisions.Exceptions.cs
@@ -9,6 +9,7 @@ using FluentAssertions;
 using LondonDataServices.IDecide.Core.Models.Foundations.Decisions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using RESTFulSense.Models;
 using Xeptions;
 
 namespace LondonDataServices.IDecide.Manage.Server.Tests.Unit.Controllers.PatientDecisions
@@ -34,6 +35,42 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Unit.Controllers.Patien
             this.decisionOrchestrationServiceMock.Setup(service =>
                 service.RetrieveAllPendingAdoptionDecisionsForConsumer(inputFrom.Value, decisionType))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<List<Decision>> actualActionResult =
+                await this.patientDecisionController.GetPatientDecisionsAsync(
+                    from: inputFrom,
+                    decisionType: decisionType);
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.decisionOrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllPendingAdoptionDecisionsForConsumer(inputFrom.Value, decisionType),
+                    Times.Once);
+
+            this.decisionOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetPatientDecisionsIfServerErrorOccurredAsync(
+            Xeption serverException)
+        {
+            // given
+            DateTimeOffset? randomDateTimeOffset = GetRandomDateTimeOffset();
+            DateTimeOffset? inputFrom = randomDateTimeOffset;
+            string randomString = GetRandomString();
+            string decisionType = randomString;
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult = new ActionResult<List<Decision>>(expectedInternalServerErrorObjectResult);
+
+            this.decisionOrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllPendingAdoptionDecisionsForConsumer(inputFrom.Value, decisionType))
+                    .ThrowsAsync(serverException); ;
 
             // when
             ActionResult<List<Decision>> actualActionResult =
