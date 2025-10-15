559ff8abafdb41572ad91f71bcc75dc30f55de6b
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-15 15:52:06 +0100

    Manage - PatientDecision ShouldGetPatientDecisions

---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Apis/PatientDecisions/PatientDecisionTests.GetPatientDecisions.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.Consumers;
+using LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.DecisionTypes;
+using LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.Patients;
+using DecisionEntity = LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.Decisions.Decision;
+using PatientDecision = LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.PatientDecisions.Decision;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Apis.PatientDecisions
+{
+    public partial class PatientDecisionTests
+    {
+        [Fact]
+        public async Task ShouldGetPatientDecisions()
+        {
+            // given
+            Patient randomPatient = await PostRandomPatientAsync();
+            DecisionType randomDecisionType = await PostRandomDecisionTypeAsync();
+            string userId = "65b5ccfb-b501-4ad5-8dd7-2a33ff64eaa3";
+
+            Consumer randomConsumerWithMatchingEntraId =
+                await PostRandomConsumerWithMatchingEntraIdEntryAsync(userId);
+
+            int decisionCount = GetRandomNumber();
+            List<PatientDecision> expectedDecisions = new List<PatientDecision>();
+
+            for (int i = 0; i < decisionCount; i++)
+            {
+                DecisionEntity randomDecision = await PostRandomDecisionAsync(randomPatient, randomDecisionType.Id);
+                randomDecision.Should().NotBeNull();
+                PatientDecision randomPatientDecision = ToPatientDecision(randomDecision, randomPatient);
+                expectedDecisions.Add(randomPatientDecision);
+            }
+
+            DateTimeOffset from = DateTimeOffset.Now.AddDays(-1);
+            string decisionType = randomDecisionType.Name;
+
+            // when
+            List<PatientDecision> actualDecisions = await this.apiBroker.GetPatientDecisionsAsync(from, decisionType);
+
+            // then
+            foreach (PatientDecision expectedDecision in expectedDecisions)
+            {
+                PatientDecision actualDecision =
+                    actualDecisions.Find(decision => decision.Id == expectedDecision.Id);
+
+                actualDecision.Should().BeEquivalentTo(expectedDecision, options => options
+                    .Excluding(property => property.Patient));
+            }
+
+            foreach (var decision in expectedDecisions)
+            {
+                await this.apiBroker.DeleteDecisionByIdAsync(decision.Id);
+            }
+
+            await this.apiBroker.DeleteConsumerByIdAsync(randomConsumerWithMatchingEntraId.Id);
+            await this.apiBroker.DeleteDecisionTypeByIdAsync(randomDecisionType.Id);
+            await this.apiBroker.DeletePatientByIdAsync(randomPatient.Id);
+        }
+    }
+}
--- LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/ApiBroker.PatientDecisions.cs
+++ LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/ApiBroker.PatientDecisions.cs
@@ -2,6 +2,8 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Models.PatientDecisions;
 
@@ -13,5 +15,16 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
 
         public async ValueTask<Decision> PostPatientDecisionAsync(Decision decision) =>
             await this.apiFactoryClient.PostContentAsync($"{patientDecisionsRelativeUrl}/PatientDecision", decision);
+
+
+        public async ValueTask<List<Decision>> GetPatientDecisionsAsync(DateTimeOffset from, string decisionType)
+        {
+            string url =
+                $"{patientDecisionsRelativeUrl}/PatientDecision?" +
+                $"from={Uri.EscapeDataString(from.ToString("o"))}&" +
+                $"decisionType={Uri.EscapeDataString(decisionType)}";
+
+            return await this.apiFactoryClient.GetContentAsync<List<Decision>>(url);
+        }
     }
 }
