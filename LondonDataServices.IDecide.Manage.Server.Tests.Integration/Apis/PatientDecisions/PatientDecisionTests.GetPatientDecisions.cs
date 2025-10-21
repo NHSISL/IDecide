25670ca3be1458f688c44150078c076064560c26
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-21 15:18:48 +0100

    INTEGRATION: PatientDecision GetPatientDecisions

---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Integration/Apis/PatientDecisions/PatientDecisionTests.GetPatientDecisions.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Manage.Server.Tests.Integration.Models.Consumers;
+using LondonDataServices.IDecide.Manage.Server.Tests.Integration.Models.Decisions;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Integration.Apis.PatientDecisions
+{
+    public partial class PatientDecisionTests
+    {
+        [Fact]
+        public async Task ShouldGetPatientDecisions()
+        {
+            // given
+            string userId = TestAuthHandler.TestUserId;
+
+            Consumer randomConsumerWithMatchingEntraId =
+                await PostRandomConsumerWithMatchingEntraIdEntryAsync(userId);
+
+            // when
+            List<Decision> actualDecisions = await this.apiBroker.GetPatientDecisionsAsync();
+
+            // then
+            actualDecisions.Should().NotBeNull();
+
+            await this.apiBroker.DeleteConsumerByIdAsync(randomConsumerWithMatchingEntraId.Id);
+        }
+    }
+}
---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Integration/Brokers/ApiBroker.PatientDecisions.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LondonDataServices.IDecide.Manage.Server.Tests.Integration.Models.Decisions;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Integration.Brokers
+{
+    public partial class ApiBroker
+    {
+        private const string PatientDecisionsRelativeUrl = "api/patientdecision";
+
+        public async ValueTask<List<Decision>> GetPatientDecisionsAsync(
+            DateTimeOffset? from = null,
+            string? decisionType = null)
+        {
+            var queryParameters = new List<string>();
+
+            if (from.HasValue)
+            {
+                queryParameters.Add(
+                    $"from={Uri.EscapeDataString(from.Value.ToString("o"))}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(decisionType))
+            {
+                queryParameters.Add(
+                    $"decisionType={Uri.EscapeDataString(decisionType)}");
+            }
+
+            string url = $"{PatientDecisionsRelativeUrl}/PatientDecision";
+
+            if (queryParameters.Count > 0)
+            {
+                url += "?" + string.Join("&", queryParameters);
+            }
+
+            return await this.apiFactoryClient.GetContentAsync<List<Decision>>(url);
+        }
+    }
+}
+
