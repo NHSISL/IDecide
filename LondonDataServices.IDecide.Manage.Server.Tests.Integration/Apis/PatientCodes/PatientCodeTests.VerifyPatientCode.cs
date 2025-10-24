b8fb8b7ec7416e9fb47326dcc06ff9137e57c266
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-24 12:55:21 +0100

    INTEGRATIONS: PatientCode

---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Integration/Apis/PatientCodes/PatientCodeTests.VerifyPatientCode.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LondonDataServices.IDecide.Manage.Server.Models;
+using LondonDataServices.IDecide.Manage.Server.Tests.Integration.Models.Patients;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Integration.Apis.PatientCodes
+{
+    public partial class PatientCodeTests
+    {
+        [Fact]
+        public async Task ShouldVerifyPatientCodeAsync()
+        {
+            // given
+            Patient randomPatient = await PostRandomPatientAsync();
+            PatientCodeRequest randomPatientCodeRequest = CreateRandomPatientCodeRequest(randomPatient);
+            PatientCodeRequest inputPatientCodeRequest = randomPatientCodeRequest.DeepClone();
+
+            // when
+            await this.apiBroker.VerifyPatientCodeAsync(inputPatientCodeRequest);
+
+            // then
+            await this.apiBroker.DeletePatientByIdAsync(randomPatient.Id);
+        }
+    }
+}
---
+++ LondonDataServices.IDecide.Portal.Server.Tests.Integration/Apis/PatientCodes/PatientCodeTests.PostPatientGenerationRequest.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LondonDataServices.IDecide.Portal.Server.Models;
+using LondonDataServices.IDecide.Portal.Server.Tests.Integration.Models.Patients;
+
+namespace LondonDataServices.IDecide.Portal.Server.Tests.Integration.Apis.PatientCodes
+{
+    public partial class PatientCodeTests
+    {
+        [Fact]
+        public async Task ShouldPostPatientGenerationRequestAsync()
+        {
+            // given
+            string nhsNumber = "9449304424";
+            Patient randomPatient = await PostRandomPatientAsync(nhsNumber);
+            PatientCodeRequest randomPatientCodeRequest = CreateRandomPatientCodeRequest(randomPatient);
+            PatientCodeRequest inputPatientCodeRequest = randomPatientCodeRequest.DeepClone();
+
+            // when
+            await this.apiBroker.PostPatientGenerationRequestAsync(inputPatientCodeRequest);
+
+            // then
+            List<Patient> patients = await this.apiBroker.GetAllPatientsAsync();
+
+            Patient addedPatient = patients
+                .First(patient => patient.NhsNumber == inputPatientCodeRequest.NhsNumber);
+
+            await this.apiBroker.DeletePatientByIdAsync(addedPatient.Id);
+        }
+    }
+}
