7c062a8da29a23e9792d8557b2e4074944a1f817
davidhayes03 <david.hayes17@nhs.net>
2025-10-24 14:16:01 +0100

    CODE RUB: Linting

--- LondonDataServices.IDecide.Manage.Client/src/components/consumerAdoptions/consumerAdoptionTable.tsx
+++ LondonDataServices.IDecide.Manage.Client/src/components/consumerAdoptions/consumerAdoptionTable.tsx
@@ -1,27 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Container, Table } from "react-bootstrap";
 import { ConsumerAdoption } from "../../models/consumerAdoptions/consumerAdoption";
-import { useEffect, useState } from "react";
 import InfiniteScroll from "../bases/pagers/InfiniteScroll";
 import { SpinnerBase } from "../bases/spinner/SpinnerBase";
 import InfiniteScrollLoader from "../bases/pagers/InfiniteScroll.Loader";
 import ConsumerAdoptionRow from "./consumerAdoptionRow";
 import { consumerAdoptionViewService } from "../../services/views/consumerAdoptionViewService";
 
-type Page = {
-    data: ConsumerAdoption[];
-};
+//type Page = {
+//    data: ConsumerAdoption[];
+//};
 
 type ConsumerAdoptionTableProps = {
     decisionId?: string;
 };
 
 const ConsumerAdoptionTable = ({ decisionId }: ConsumerAdoptionTableProps) => {
-    if (!decisionId) {
-        return null;
-    }
-
-    const [showSpinner] = useState(false);
     const [mappedConsumerAdoption, setMappedConsumerAdoption] = useState<Array<ConsumerAdoption>>([]);
     const [totalPages, setTotalPages] = useState<number>(0);
 
@@ -34,94 +28,81 @@ const ConsumerAdoptionTable = ({ decisionId }: ConsumerAdoptionTableProps) => {
     } = consumerAdoptionViewService.useGetAllConsumerAdoptions(decisionId);
 
     useEffect(() => {
-        if (pages && Array.isArray(pages)) {
-            const allData = extractAllData(pages);
+        if (Array.isArray(pages)) {
+            const allData = pages.flatMap(page => Array.isArray(page.data) ? page.data : []);
             setMappedConsumerAdoption(allData);
-            updateTotalPages(pages, allData);
+
+            const itemsPerPage = pages[0]?.data.length || 1;
+            const totalItems = allData.length;
+            setTotalPages(Math.ceil(totalItems / itemsPerPage));
         }
     }, [pages]);
-
-    const extractAllData = (pages: Page[]): ConsumerAdoption[] => {
-        return pages.flatMap(page => {
-            if (Array.isArray(page.data)) {
-                return page.data;
-            } else {
-                return [];
-            }
-        });
-    };
-
-    const updateTotalPages = (pages: Page[], uniqueAccessAudit: Array<ConsumerAdoption>) => {
-        const itemsPerPage = pages[0]?.data.length || 1;
-        const totalItems = uniqueAccessAudit.length;
-        setTotalPages(Math.ceil(totalItems / itemsPerPage));
-    };
 
     const hasNoMorePages = (): boolean => {
         return !isLoading && !hasNextPage;
     };
 
+    if (!decisionId) {
+        return null;
+    }
+
     return (
-        <>
-            <Container fluid className="infiniteScrollContainer">
-                <Card>
-                    <Card.Header>
-                        <span style={{ fontSize: "30px" }}>Consumer Adoptions</span>
-                    </Card.Header>
-                    <Card.Body>
-                        <InfiniteScroll
-                            loading={isLoading || showSpinner}
-                            hasNextPage={hasNextPage || false}
-                            loadMore={fetchNextPage}
-                        >
-                            <Table striped bordered hover variant="light" responsive>
-                                <thead>
+        <Container fluid className="infiniteScrollContainer">
+            <Card>
+                <Card.Header>
+                    <span style={{ fontSize: "30px" }}>Consumer Adoptions</span>
+                </Card.Header>
+                <Card.Body>
+                    <InfiniteScroll
+                        loading={isLoading}
+                        hasNextPage={hasNextPage || false}
+                        loadMore={fetchNextPage}
+                    >
+                        <Table striped bordered hover variant="light" responsive>
+                            <thead>
+                                <tr>
+                                    <th className="text-center">Consumer</th>
+                                    <th className="text-center">Decison</th>
+                                    <th className="text-center">Decison Datetime</th>
+                                    <th className="text-center">Adoption DateTime</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {isLoading ? (
                                     <tr>
-                                        <th className="text-center">Consumer</th>
-                                        <th className="text-center">Decison</th>
-                                        <th className="text-center">Decison Datetime</th>
-                                        <th className="text-center">Adoption DateTime</th>
+                                        <td colSpan={6} className="text-center">
+                                            <SpinnerBase />
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    {isLoading || showSpinner ? (
+                                ) : (
+                                    <>
+                                        {mappedConsumerAdoption.map(
+                                            (consumerAdoption: ConsumerAdoption) => (
+                                                <ConsumerAdoptionRow
+                                                    key={consumerAdoption.id}
+                                                    consumerAdoption={consumerAdoption}
+                                                />
+                                            )
+                                        )}
                                         <tr>
                                             <td colSpan={6} className="text-center">
-                                                <SpinnerBase />
+                                                <InfiniteScrollLoader
+                                                    loading={isFetchingNextPage}
+                                                    spinner={<SpinnerBase />}
+                                                    noMorePages={!hasNoMorePages()}
+                                                    noMorePagesMessage={<>-- No more pages --</>}
+                                                    totalPages={totalPages}
+                                                />
                                             </td>
                                         </tr>
-                                    ) : (
-                                        <>
-                                            {(
-                                                mappedConsumerAdoption && mappedConsumerAdoption.map(
-                                                    (consumerAdoption: ConsumerAdoption) => (
-                                                        <ConsumerAdoptionRow
-                                                            key={consumerAdoption.id}
-                                                            consumerAdoption={consumerAdoption}
-                                                        />
-                                                    )
-                                                )
-                                            )}
-                                            <tr>
-                                                <td colSpan={6} className="text-center">
-                                                    <InfiniteScrollLoader
-                                                        loading={isFetchingNextPage}
-                                                        spinner={<SpinnerBase />}
-                                                        noMorePages={!hasNoMorePages()}
-                                                        noMorePagesMessage={<>-- No more pages --</>}
-                                                        totalPages={totalPages}
-                                                    />
-                                                </td>
-                                            </tr>
-                                        </>
-                                    )}
-                                </tbody>
-                            </Table>
-                        </InfiniteScroll>
-                    </Card.Body>
-                </Card>
-            </Container>
-        </>
+                                    </>
+                                )}
+                            </tbody>
+                        </Table>
+                    </InfiniteScroll>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 };
 
