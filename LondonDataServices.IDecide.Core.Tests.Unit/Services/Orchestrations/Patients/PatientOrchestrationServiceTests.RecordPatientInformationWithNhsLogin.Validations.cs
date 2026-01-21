58359150cafc63d1a7055e74c652ad7da4d59633
davidhayes03 <david.hayes17@nhs.net>
2026-01-21 10:05:39 +0000

    ShouldThrowValidationExceptionOnRecordPatientInformationWithNhsLoginAsyncWithInvalidNhsNumber -> PASS

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/Patients/PatientOrchestrationServiceTests.RecordPatientInformationWithNhsLogin.Validations.cs
@@ -0,0 +1,87 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LondonDataServices.IDecide.Core.Models.Foundations.Notifications;
+using LondonDataServices.IDecide.Core.Models.Orchestrations.Patients.Exceptions;
+using LondonDataServices.IDecide.Core.Services.Orchestrations.Patients;
+using Moq;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Patients
+{
+    public partial class PatientOrchestrationServiceTests
+    {
+        [Theory]
+        [InlineData("123456789")]
+        [InlineData("01234567890")]
+        public async Task ShouldThrowValidationExceptionOnRecordPatientInformationWithNhsLoginAsyncWithInvalidNhsNumber(
+            string invalidNhsNumber)
+        {
+            // given
+            NotificationPreference randomNotificationPreference = NotificationPreference.Email;
+            NotificationPreference inputNotificationPreference = randomNotificationPreference.DeepClone();
+            string notificationPreferenceString = inputNotificationPreference.ToString();
+
+            var invalidPatientOrchestrationArgumentException =
+                new InvalidPatientOrchestrationArgumentException(
+                    "Invalid patient orchestration argument. Please correct the errors and try again.");
+
+            invalidPatientOrchestrationArgumentException.AddData(
+                key: "nhsNumber",
+                values: "Text must be exactly 10 digits.");
+
+            var expectedPatientOrchestrationValidationException =
+                new PatientOrchestrationValidationException(
+                    message: "Patient orchestration validation error occurred, please fix the errors and try again.",
+                    innerException: invalidPatientOrchestrationArgumentException);
+
+            var patientOrchestrationServiceMock = new Mock<PatientOrchestrationService>(
+                this.loggingBrokerMock.Object,
+                this.securityBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.auditBrokerMock.Object,
+                this.identifierBrokerMock.Object,
+                this.pdsServiceMock.Object,
+                this.patientServiceMock.Object,
+                this.notificationServiceMock.Object,
+                this.decisionConfigurations,
+                this.securityBrokerConfigurations)
+            { CallBase = true };
+
+            patientServiceMock.Setup(service =>
+                service.RetrieveAllPatientsAsync())
+                    .ThrowsAsync(invalidPatientOrchestrationArgumentException);
+
+            // when
+            ValueTask recordPatientInformationTask =
+                patientOrchestrationServiceMock.Object.RecordPatientInformationAsync(
+                    invalidNhsNumber,
+                    notificationPreferenceString,
+                    false);
+
+            PatientOrchestrationValidationException actualException =
+                await Assert.ThrowsAsync<PatientOrchestrationValidationException>(
+                    recordPatientInformationTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedPatientOrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedPatientOrchestrationValidationException))),
+                       Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+            this.pdsServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.notificationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
