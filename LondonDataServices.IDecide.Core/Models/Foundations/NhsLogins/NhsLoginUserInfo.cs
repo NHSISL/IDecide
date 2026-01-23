8845b2369000a1e1a11465a7c475805df4b3208d
Christo du Toit <christo@dutoit.co.uk>
2026-01-23 15:25:40 +0000

    CODE RUB: Code cleanup

--- LondonDataServices.IDecide.Core/Models/Foundations/NhsLogins/NhsLoginUserInfo.cs
+++ LondonDataServices.IDecide.Core/Models/Foundations/NhsLogins/NhsLoginUserInfo.cs
@@ -7,12 +7,16 @@ namespace LondonDataServices.IDecide.Core.Models.Foundations.NhsLogins
     {
         [JsonPropertyName("birthdate")]
         public DateTime Birthdate { get; set; }
+
         [JsonPropertyName("family_name")]
         public string FamilyName { get; set; }
+        
         [JsonPropertyName("email")]
         public string Email { get; set; }
+        
         [JsonPropertyName("phone_number")]
         public string PhoneNumber { get; set; }
+        
         [JsonPropertyName("given_name")]
         public string GivenName { get; set; }
     }
