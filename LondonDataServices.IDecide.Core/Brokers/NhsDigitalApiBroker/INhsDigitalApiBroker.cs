d3ee0768c80c07335e5394e0e49c796a70df476f
davidhayes03 <david.hayes17@nhs.net>
2026-04-23 10:52:53 +0100

    BROKER: NhsDigitalApiBroker

---
+++ LondonDataServices.IDecide.Core/Brokers/NhsDigitalApiBroker/INhsDigitalApiBroker.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using NHSDigital.ApiPlatform.Sdk.Models.Foundations.Pds;
+
+namespace LondonDataServices.IDecide.Core.Brokers.NhsDigitalApi
+{
+    public interface INhsDigitalApiBroker
+    {
+        ValueTask<string> SearchPatientPDSAsync(
+            SearchCriteria searchCriteria,
+            CancellationToken cancellationToken);
+    }
+}
