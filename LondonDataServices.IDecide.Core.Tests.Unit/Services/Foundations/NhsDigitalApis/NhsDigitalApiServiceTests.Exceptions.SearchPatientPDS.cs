54f1b6d4afdf643fadd771bf9016cf9d2ed46a08
davidhayes03 <david.hayes17@nhs.net>
2026-04-27 10:18:35 +0100

    ShouldThrowServiceExceptionOnSearchPatientPDSIfServiceErrorOccursAndLogItAsync -> FAIL

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/NhsDigitalApis/NhsDigitalApiServiceTests.Exceptions.SearchPatientPDS.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/NhsDigitalApis/NhsDigitalApiServiceTests.Exceptions.SearchPatientPDS.cs
@@ -3,6 +3,8 @@
 // ---------------------------------------------------------
 
 using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -62,6 +64,67 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.NhsDig
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedNhsDigitalApiServiceException))),
+                Times.Once);
+
+            this.nhsDigitalApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnSearchPatientPDSIfClientErrorOccursAndLogItAsync()
+        {
+            // given
+            SearchCriteria randomSearchCriteria = CreateRandomSearchCriteria();
+            CancellationToken inputCancellationToken = GetCancellationToken();
+
+            var httpRequestException =
+                new HttpRequestException(
+                    message: GetRandomString(),
+                    inner: null,
+                    statusCode: HttpStatusCode.BadRequest);
+
+            var clientNhsDigitalApiException =
+                new ClientNhsDigitalApiException(
+                    message: "NhsDigitalApi client error occurred, please fix the errors and try again.",
+                    innerException: httpRequestException,
+                    data: httpRequestException.Data);
+
+            var expectedNhsDigitalApiDependencyValidationException =
+                new NhsDigitalApiDependencyValidationException(
+                    message: "NhsDigitalApi dependency validation error occurred, " +
+                        "please fix the errors and try again.",
+                    innerException: clientNhsDigitalApiException);
+
+            this.nhsDigitalApiBrokerMock.Setup(broker =>
+                broker.SearchPatientPDSAsync(
+                    randomSearchCriteria,
+                    inputCancellationToken))
+                        .ThrowsAsync(httpRequestException);
+
+            // when
+            ValueTask<string> searchPatientPDSTask =
+                this.nhsDigitalApiService.SearchPatientPDSAsync(
+                    randomSearchCriteria,
+                    inputCancellationToken);
+
+            NhsDigitalApiDependencyValidationException actualException =
+                await Assert.ThrowsAsync<NhsDigitalApiDependencyValidationException>(
+                    testCode: searchPatientPDSTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(
+                expectedNhsDigitalApiDependencyValidationException);
+
+            this.nhsDigitalApiBrokerMock.Verify(broker =>
+                broker.SearchPatientPDSAsync(
+                    randomSearchCriteria,
+                    inputCancellationToken),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNhsDigitalApiDependencyValidationException))),
                 Times.Once);
 
             this.nhsDigitalApiBrokerMock.VerifyNoOtherCalls();
