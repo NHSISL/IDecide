445558417aa3d3b2a8ece5c5d9ecb154a1076291
davidhayes03 <david.hayes17@nhs.net>
2026-04-28 10:52:48 +0100

    ShouldThrowServiceExceptionOnLogoutIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Foundations/NhsDigitalApis/NhsDigitalApiServiceTests.Exceptions.Logout.cs
@@ -0,0 +1,65 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonDataServices.IDecide.Core.Models.Foundations.NhsDigitalApis.Exceptions;
+using Moq;
+using Task = System.Threading.Tasks.Task;
+
+namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Foundations.NhsDigitalApis
+{
+    public partial class NhsDigitalApiServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnLogoutIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken inputCancellationToken = GetCancellationToken();
+            var serviceException = new Exception();
+
+            var failedNhsDigitalApiServiceException =
+                new FailedNhsDigitalApiServiceException(
+                    message: "Failed NhsDigitalApi service error occurred, please contact support.",
+                    innerException: serviceException);
+
+            var expectedNhsDigitalApiServiceException =
+                new NhsDigitalApiServiceException(
+                    message: "NhsDigitalApi service error occurred, please contact support.",
+                    innerException: failedNhsDigitalApiServiceException);
+
+            this.nhsDigitalApiBrokerMock.Setup(broker =>
+                broker.LogoutAsync(inputCancellationToken))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask logoutTask =
+                this.nhsDigitalApiService.LogoutAsync(inputCancellationToken);
+
+            NhsDigitalApiServiceException actualNhsDigitalApiServiceException =
+                await Assert.ThrowsAsync<NhsDigitalApiServiceException>(
+                    testCode: logoutTask.AsTask);
+
+            // then
+            actualNhsDigitalApiServiceException.Should().BeEquivalentTo(
+                expectedNhsDigitalApiServiceException);
+
+            this.nhsDigitalApiBrokerMock.Verify(broker =>
+                broker.LogoutAsync(inputCancellationToken),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNhsDigitalApiServiceException))),
+                Times.Once);
+
+            this.nhsDigitalApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
