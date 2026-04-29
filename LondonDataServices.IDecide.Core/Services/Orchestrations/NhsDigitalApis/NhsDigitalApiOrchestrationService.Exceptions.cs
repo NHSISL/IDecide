1e8eed7d646a2fd10fb4ca1b497592e252b003fa
davidhayes03 <david.hayes17@nhs.net>
2026-04-29 11:11:58 +0100

    ShouldThrowDependencyExceptionOnProcessCallbackAndLogItAsync -> PASS ShouldThrowDependencyValidationExceptionOnProcessCallbackAndLogItAsync -> PASS

--- LondonDataServices.IDecide.Core/Services/Orchestrations/NhsDigitalApis/NhsDigitalApiOrchestrationService.Exceptions.cs
+++ LondonDataServices.IDecide.Core/Services/Orchestrations/NhsDigitalApis/NhsDigitalApiOrchestrationService.Exceptions.cs
@@ -3,6 +3,8 @@
 // ---------------------------------------------------------
 
 using System.Threading.Tasks;
+using LondonDataServices.IDecide.Core.Models.Foundations.NhsDigitalApis.Exceptions;
+using LondonDataServices.IDecide.Core.Models.Foundations.Users.Exceptions;
 using LondonDataServices.IDecide.Core.Models.Orchestrations.NhsDigitalApis.Exceptions;
 using Xeptions;
 
@@ -23,6 +25,40 @@ namespace LondonDataServices.IDecide.Core.Services.Orchestrations.NhsDigitalApis
             {
                 throw await CreateAndLogValidationExceptionAsync(
                     invalidNhsDigitalApiOrchestrationArgumentException);
+            }
+            catch (NhsDigitalApiValidationException nhsDigitalApiValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    nhsDigitalApiValidationException);
+            }
+            catch (NhsDigitalApiDependencyValidationException nhsDigitalApiDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    nhsDigitalApiDependencyValidationException);
+            }
+            catch (UserValidationException userValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(userValidationException);
+            }
+            catch (UserDependencyValidationException userDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(userDependencyValidationException);
+            }
+            catch (NhsDigitalApiDependencyException nhsDigitalApiDependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(nhsDigitalApiDependencyException);
+            }
+            catch (NhsDigitalApiServiceException nhsDigitalApiServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(nhsDigitalApiServiceException);
+            }
+            catch (UserDependencyException userDependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(userDependencyException);
+            }
+            catch (UserServiceException userServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(userServiceException);
             }
             catch (System.Exception exception)
             {
@@ -49,6 +85,34 @@ namespace LondonDataServices.IDecide.Core.Services.Orchestrations.NhsDigitalApis
             await this.loggingBroker.LogErrorAsync(nhsDigitalApiOrchestrationValidationException);
 
             return nhsDigitalApiOrchestrationValidationException;
+        }
+
+        private async ValueTask<NhsDigitalApiOrchestrationDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var nhsDigitalApiOrchestrationDependencyValidationException =
+                new NhsDigitalApiOrchestrationDependencyValidationException(
+                    message: "NhsDigitalApi orchestration dependency validation error occurred, " +
+                        "please fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(nhsDigitalApiOrchestrationDependencyValidationException);
+
+            return nhsDigitalApiOrchestrationDependencyValidationException;
+        }
+
+        private async ValueTask<NhsDigitalApiOrchestrationDependencyException>
+            CreateAndLogDependencyExceptionAsync(Xeption exception)
+        {
+            var nhsDigitalApiOrchestrationDependencyException =
+                new NhsDigitalApiOrchestrationDependencyException(
+                    message: "NhsDigitalApi orchestration dependency error occurred, " +
+                        "please fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(nhsDigitalApiOrchestrationDependencyException);
+
+            return nhsDigitalApiOrchestrationDependencyException;
         }
 
         private async ValueTask<NhsDigitalApiOrchestrationServiceException>
