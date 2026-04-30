d74d248f48002049aee894de0ee5825d6bcf1604
davidhayes03 <david.hayes17@nhs.net>
2026-04-30 14:56:29 +0100

    CODE RUB: Format

--- LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/NhsDigitalApis/NhsDigitalApiOrchestrationServiceTests.BuildLoginUrl.Exceptions.cs
+++ LondonDataServices.IDecide.Core.Tests.Unit/Services/Orchestrations/NhsDigitalApis/NhsDigitalApiOrchestrationServiceTests.BuildLoginUrl.Exceptions.cs
@@ -59,134 +59,134 @@ namespace LondonDataServices.IDecide.Core.Tests.Unit.Services.Orchestrations.Nhs
             this.userServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
-            [Theory]
-            [MemberData(nameof(NhsDigitalApiDependencyExceptions))]
-            public async Task ShouldThrowDependencyExceptionOnBuildLoginUrlAndLogItAsync(
-                Xeption dependencyException)
-            {
-                // given
-                CancellationToken inputCancellationToken = GetCancellationToken();
+        [Theory]
+        [MemberData(nameof(NhsDigitalApiDependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnBuildLoginUrlAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            CancellationToken inputCancellationToken = GetCancellationToken();
 
-                this.nhsDigitalApiServiceMock.Setup(service =>
-                    service.BuildLoginUrlAsync(inputCancellationToken))
-                        .ThrowsAsync(dependencyException);
+            this.nhsDigitalApiServiceMock.Setup(service =>
+                service.BuildLoginUrlAsync(inputCancellationToken))
+                    .ThrowsAsync(dependencyException);
 
-                var expectedNhsDigitalApiOrchestrationDependencyException =
-                    new NhsDigitalApiOrchestrationDependencyException(
-                        message: "NhsDigitalApi orchestration dependency error occurred, " +
-                            "please fix the errors and try again.",
-                        innerException: dependencyException);
+            var expectedNhsDigitalApiOrchestrationDependencyException =
+                new NhsDigitalApiOrchestrationDependencyException(
+                    message: "NhsDigitalApi orchestration dependency error occurred, " +
+                        "please fix the errors and try again.",
+                    innerException: dependencyException);
 
-                // when
-                ValueTask<string> buildLoginUrlTask =
-                    this.nhsDigitalApiOrchestrationService
-                        .BuildLoginUrlAsync(inputCancellationToken);
+            // when
+            ValueTask<string> buildLoginUrlTask =
+                this.nhsDigitalApiOrchestrationService
+                    .BuildLoginUrlAsync(inputCancellationToken);
 
-                NhsDigitalApiOrchestrationDependencyException
-                    actualNhsDigitalApiOrchestrationDependencyException =
-                        await Assert.ThrowsAsync<NhsDigitalApiOrchestrationDependencyException>(
-                            testCode: buildLoginUrlTask.AsTask);
+            NhsDigitalApiOrchestrationDependencyException
+                actualNhsDigitalApiOrchestrationDependencyException =
+                    await Assert.ThrowsAsync<NhsDigitalApiOrchestrationDependencyException>(
+                        testCode: buildLoginUrlTask.AsTask);
 
-                // then
-                actualNhsDigitalApiOrchestrationDependencyException
-                    .Should().BeEquivalentTo(expectedNhsDigitalApiOrchestrationDependencyException);
+            // then
+            actualNhsDigitalApiOrchestrationDependencyException
+                .Should().BeEquivalentTo(expectedNhsDigitalApiOrchestrationDependencyException);
 
-                this.nhsDigitalApiServiceMock.Verify(service =>
-                    service.BuildLoginUrlAsync(inputCancellationToken),
+            this.nhsDigitalApiServiceMock.Verify(service =>
+                service.BuildLoginUrlAsync(inputCancellationToken),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNhsDigitalApiOrchestrationDependencyException))),
                         Times.Once);
 
-                this.loggingBrokerMock.Verify(broker =>
-                    broker.LogErrorAsync(It.Is(SameExceptionAs(
-                        expectedNhsDigitalApiOrchestrationDependencyException))),
-                            Times.Once);
+            this.nhsDigitalApiServiceMock.VerifyNoOtherCalls();
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
 
-                this.nhsDigitalApiServiceMock.VerifyNoOtherCalls();
-                this.userServiceMock.VerifyNoOtherCalls();
-                this.loggingBrokerMock.VerifyNoOtherCalls();
-            }
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnBuildLoginUrlIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken inputCancellationToken = GetCancellationToken();
+            var serviceException = new Exception();
 
-            [Fact]
-            public async Task ShouldThrowServiceExceptionOnBuildLoginUrlIfServiceErrorOccursAndLogItAsync()
-            {
-                // given
-                CancellationToken inputCancellationToken = GetCancellationToken();
-                var serviceException = new Exception();
+            this.nhsDigitalApiServiceMock.Setup(service =>
+                service.BuildLoginUrlAsync(inputCancellationToken))
+                    .ThrowsAsync(serviceException);
 
-                this.nhsDigitalApiServiceMock.Setup(service =>
-                    service.BuildLoginUrlAsync(inputCancellationToken))
-                        .ThrowsAsync(serviceException);
+            var failedNhsDigitalApiOrchestrationServiceException =
+                new FailedNhsDigitalApiOrchestrationServiceException(
+                    message: "Failed NhsDigitalApi orchestration service error occurred, " +
+                        "please contact support.",
+                    innerException: serviceException);
 
-                var failedNhsDigitalApiOrchestrationServiceException =
-                    new FailedNhsDigitalApiOrchestrationServiceException(
-                        message: "Failed NhsDigitalApi orchestration service error occurred, " +
-                            "please contact support.",
-                        innerException: serviceException);
+            var expectedNhsDigitalApiOrchestrationServiceException =
+                new NhsDigitalApiOrchestrationServiceException(
+                    message: "NhsDigitalApi orchestration service error occurred, please contact support.",
+                    innerException: failedNhsDigitalApiOrchestrationServiceException);
 
-                var expectedNhsDigitalApiOrchestrationServiceException =
-                    new NhsDigitalApiOrchestrationServiceException(
-                        message: "NhsDigitalApi orchestration service error occurred, please contact support.",
-                        innerException: failedNhsDigitalApiOrchestrationServiceException);
+            // when
+            ValueTask<string> buildLoginUrlTask =
+                this.nhsDigitalApiOrchestrationService
+                    .BuildLoginUrlAsync(inputCancellationToken);
 
-                // when
-                ValueTask<string> buildLoginUrlTask =
-                    this.nhsDigitalApiOrchestrationService
-                        .BuildLoginUrlAsync(inputCancellationToken);
+            NhsDigitalApiOrchestrationServiceException
+                actualNhsDigitalApiOrchestrationServiceException =
+                    await Assert.ThrowsAsync<NhsDigitalApiOrchestrationServiceException>(
+                        testCode: buildLoginUrlTask.AsTask);
 
-                NhsDigitalApiOrchestrationServiceException
-                    actualNhsDigitalApiOrchestrationServiceException =
-                        await Assert.ThrowsAsync<NhsDigitalApiOrchestrationServiceException>(
-                            testCode: buildLoginUrlTask.AsTask);
+            // then
+            actualNhsDigitalApiOrchestrationServiceException
+                .Should().BeEquivalentTo(expectedNhsDigitalApiOrchestrationServiceException);
 
-                // then
-                actualNhsDigitalApiOrchestrationServiceException
-                    .Should().BeEquivalentTo(expectedNhsDigitalApiOrchestrationServiceException);
+            this.nhsDigitalApiServiceMock.Verify(service =>
+                service.BuildLoginUrlAsync(inputCancellationToken),
+                    Times.Once);
 
-                this.nhsDigitalApiServiceMock.Verify(service =>
-                    service.BuildLoginUrlAsync(inputCancellationToken),
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNhsDigitalApiOrchestrationServiceException))),
                         Times.Once);
 
-                this.loggingBrokerMock.Verify(broker =>
-                    broker.LogErrorAsync(It.Is(SameExceptionAs(
-                        expectedNhsDigitalApiOrchestrationServiceException))),
-                            Times.Once);
+            this.nhsDigitalApiServiceMock.VerifyNoOtherCalls();
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
 
-                this.nhsDigitalApiServiceMock.VerifyNoOtherCalls();
-                this.userServiceMock.VerifyNoOtherCalls();
-                this.loggingBrokerMock.VerifyNoOtherCalls();
-            }
+        [Fact]
+        public async Task ShouldPropagateCancellationOnBuildLoginUrlWhenCancelledAsync()
+        {
+            // given
+            var cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken inputCancellationToken = cancellationTokenSource.Token;
+            cancellationTokenSource.Cancel();
 
-            [Fact]
-            public async Task ShouldPropagateCancellationOnBuildLoginUrlWhenCancelledAsync()
-            {
-                // given
-                var cancellationTokenSource = new CancellationTokenSource();
-                CancellationToken inputCancellationToken = cancellationTokenSource.Token;
-                cancellationTokenSource.Cancel();
+            this.nhsDigitalApiServiceMock.Setup(service =>
+                service.BuildLoginUrlAsync(inputCancellationToken))
+                    .ThrowsAsync(new OperationCanceledException(inputCancellationToken));
 
-                this.nhsDigitalApiServiceMock.Setup(service =>
-                    service.BuildLoginUrlAsync(inputCancellationToken))
-                        .ThrowsAsync(new OperationCanceledException(inputCancellationToken));
+            // when
+            ValueTask<string> buildLoginUrlTask =
+                this.nhsDigitalApiOrchestrationService
+                    .BuildLoginUrlAsync(inputCancellationToken);
 
-                // when
-                ValueTask<string> buildLoginUrlTask =
-                    this.nhsDigitalApiOrchestrationService
-                        .BuildLoginUrlAsync(inputCancellationToken);
+            // then
+            await Assert.ThrowsAnyAsync<OperationCanceledException>(
+                testCode: buildLoginUrlTask.AsTask);
 
-                // then
-                await Assert.ThrowsAnyAsync<OperationCanceledException>(
-                    testCode: buildLoginUrlTask.AsTask);
+            this.nhsDigitalApiServiceMock.Verify(service =>
+                service.BuildLoginUrlAsync(inputCancellationToken),
+                    Times.Once);
 
-                this.nhsDigitalApiServiceMock.Verify(service =>
-                    service.BuildLoginUrlAsync(inputCancellationToken),
-                        Times.Once);
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.IsAny<Exception>()),
+                    Times.Never);
 
-                this.loggingBrokerMock.Verify(broker =>
-                    broker.LogErrorAsync(It.IsAny<Exception>()),
-                        Times.Never);
-
-                this.nhsDigitalApiServiceMock.VerifyNoOtherCalls();
-                this.userServiceMock.VerifyNoOtherCalls();
-                this.loggingBrokerMock.VerifyNoOtherCalls();
-            }
+            this.nhsDigitalApiServiceMock.VerifyNoOtherCalls();
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
+}
