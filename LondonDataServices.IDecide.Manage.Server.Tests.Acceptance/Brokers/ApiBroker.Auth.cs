e13f0c0dd7c61d59acf5fd2d5de25a1af93cac3c
davidhayes03 <david.hayes17@nhs.net>
2026-04-30 16:26:40 +0100

    CODE RUB: Add Aceeptance

---
+++ LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/ApiBroker.Auth.cs
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        private const string AuthRelativeUrl = "api/auth";
+
+        public async ValueTask<HttpResponseMessage> GetLoginRedirectAsync() =>
+            await this.httpClientNoRedirect.GetAsync($"{AuthRelativeUrl}/login");
+    }
+}
--- LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/TestWebApplicationFactory.cs
+++ LondonDataServices.IDecide.Manage.Server.Tests.Acceptance/Brokers/TestWebApplicationFactory.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using Attrify.InvisibleApi.Models;
 using ISL.Providers.Notifications.Abstractions;
 using ISL.Providers.PDS.Abstractions;
+using LondonDataServices.IDecide.Core.Brokers.NhsDigitalApi;
+using LondonDataServices.IDecide.Core.Services.Foundations.NhsDigitalApis;
+using LondonDataServices.IDecide.Core.Services.Orchestrations.NhsDigitalApis;
 using ISL.Providers.PDS.FakeFHIR.Models;
 using ISL.Providers.PDS.FakeFHIR.Providers.FakeFHIR;
 using Microsoft.AspNetCore.Authentication;
@@ -21,6 +24,9 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
 {
     public class TestWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
+        public Mock<INhsDigitalApiOrchestrationService> NhsDigitalApiOrchestrationServiceMock { get; } =
+            new Mock<INhsDigitalApiOrchestrationService>();
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureAppConfiguration((context, config) =>
@@ -40,6 +46,9 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
                 OverrideSecurityForTesting(services);
                 OverrideFhirProviderForTesting(services, context.Configuration);
                 OverrideNotificationProviderForTesting(services, context.Configuration);
+                OverrideNhsDigitalApiBrokerForTesting(services);
+                OverrideNhsDigitalApiServiceForTesting(services);
+                OverrideNhsDigitalApiOrchestrationServiceForTesting(services);
             });
         }
 
@@ -120,6 +129,48 @@ namespace LondonDataServices.IDecide.Manage.Server.Tests.Acceptance.Brokers
 
             services.AddTransient<INotificationAbstractionProvider>(
                 serviceProvider => mockNotificationAbstractionProvider.Object);
+        }
+
+        private void OverrideNhsDigitalApiOrchestrationServiceForTesting(IServiceCollection services)
+        {
+            var descriptor = services
+                .FirstOrDefault(d => d.ServiceType == typeof(INhsDigitalApiOrchestrationService));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddTransient<INhsDigitalApiOrchestrationService>(
+                serviceProvider => NhsDigitalApiOrchestrationServiceMock.Object);
+        }
+
+        private static void OverrideNhsDigitalApiBrokerForTesting(IServiceCollection services)
+        {
+            var descriptor = services
+                .FirstOrDefault(d => d.ServiceType == typeof(INhsDigitalApiBroker));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            var mockBroker = new Mock<INhsDigitalApiBroker>();
+            services.AddTransient<INhsDigitalApiBroker>(serviceProvider => mockBroker.Object);
+        }
+
+        private static void OverrideNhsDigitalApiServiceForTesting(IServiceCollection services)
+        {
+            var descriptor = services
+                .FirstOrDefault(d => d.ServiceType == typeof(INhsDigitalApiService));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            var mockService = new Mock<INhsDigitalApiService>();
+            services.AddTransient<INhsDigitalApiService>(serviceProvider => mockService.Object);
         }
     }
 }
