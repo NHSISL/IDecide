e60b82b42abffa6405afdd40df97877c22c1ce94
davidhayes03 <david.hayes17@nhs.net>
2026-06-09 15:58:48 +0100

    CODE RUB: Lint issue

--- LondonDataServices.IDecide.Manage.Client/tests/auth.setup.ts
+++ LondonDataServices.IDecide.Manage.Client/tests/auth.setup.ts
@@ -1,8 +1,5 @@
 import { test as setup } from '@playwright/test';
-import path from 'path';
 import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
 
 setup('authenticate', async () => {
     setup.skip(true, 'Skipped temporarily');
