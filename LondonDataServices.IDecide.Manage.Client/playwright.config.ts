689677b996eb24e497a95629dd93f0240b077706
davidhayes03 <david.hayes17@nhs.net>
2026-06-26 12:34:53 +0100

    CODE RUB: Lint Issues

--- LondonDataServices.IDecide.Manage.Client/playwright.config.ts
+++ LondonDataServices.IDecide.Manage.Client/playwright.config.ts
@@ -1,11 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
-const authFile = path.join(__dirname, './playwright/.auth/user.json');
 
 /**
  * See https://playwright.dev/docs/test-configuration.
